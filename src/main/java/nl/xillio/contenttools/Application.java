d5122ed7600de5748b35ffe40125584c66535ce2
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-07-01 14:13:27 +0200

    extractValue now resolves circular referenced lists

--- src/main/java/nl/xillio/contenttools/Application.java
+++ src/main/java/nl/xillio/contenttools/Application.java
@@ -3,71 +3,70 @@ package nl.xillio.contenttools;
 import java.io.File;
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import javafx.stage.Stage;
 import nl.xillio.plugins.CircularReferenceException;
 import nl.xillio.plugins.PluginLoader;
 import nl.xillio.xill.api.Xill;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
 /**
  * This is the main class of the contenttools application
  */
-public class Application extends javafx.application.Application{
-	private static List<PluginPackage> plugins;
-	private static final Logger logger = LogManager.getLogger(Application.class);
+public class Application extends javafx.application.Application {
+    private static List<PluginPackage> plugins;
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
-	/**
-	 * Main method
-	 * @param args
-	 */
-	public static void main(String... args) {
-		PluginLoader<PluginPackage> loader = PluginLoader.load(PluginPackage.class);
-		
-		File pluginFolder = new File("plugins");
-		pluginFolder.mkdirs();
-		loader.addFolder(pluginFolder);
-		
-		
-		try {
-			loader.load();
-		} catch (CircularReferenceException e) {
-			throw new RuntimeException("Error while loading plugins.", e);
-		}
-		
-		logger.info("Loaded " + loader.getPluginManager().getPlugins().size() + " plugins");
-		
-		plugins = loader.getPluginManager().getPlugins();
-		
-		if(plugins.size() < 2) {
-			throw new RuntimeException("Could not find the Xill and Contenttools plugins.");
-		}
-		
-		launch(args);
-		
+    /**
+     * Main method
+     * 
+     * @param args
+     */
+    public static void main(final String... args) {
+	PluginLoader<PluginPackage> loader = PluginLoader.load(PluginPackage.class);
+
+	File pluginFolder = new File("plugins");
+	pluginFolder.mkdirs();
+	loader.addFolder(pluginFolder);
+
+	try {
+	    loader.load();
+	} catch (CircularReferenceException e) {
+	    throw new RuntimeException("Error while loading plugins.", e);
 	}
 
-	private Xill xill;
+	logger.info("Loaded " + loader.getPluginManager().getPlugins().size() + " plugins");
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		
-		//Find served objects
-		plugins.forEach(plugin -> {
-			Object served = plugin.serve();
-			
-			if(served instanceof Xill) {
-				xill = (Xill)served;
-			}
-		});
-		
-		//Boot plugins
-		plugins.forEach(plugin -> { 
-			plugin.start(stage, xill);
-		});
-		
+	plugins = loader.getPluginManager().getPlugins();
 
-		setUserAgentStylesheet(STYLESHEET_MODENA);
+	if (plugins.size() < 2) {
+	    throw new RuntimeException("Could not find the Xill and Contenttools plugins.");
 	}
+
+	launch(args);
+
+    }
+
+    private Xill xill;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+
+	// Find served objects
+	plugins.forEach(plugin -> {
+	    Object served = plugin.serve();
+
+	    if (served instanceof Xill) {
+		xill = (Xill) served;
+	    }
+	});
+
+	// Boot plugins
+	plugins.forEach(plugin -> {
+	    plugin.start(stage, xill);
+	});
+
+	setUserAgentStylesheet(STYLESHEET_MODENA);
+    }
 }
