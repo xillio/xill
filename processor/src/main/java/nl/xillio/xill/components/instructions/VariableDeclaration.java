b14b78517c6bb24a2f9c276ef28eade773643271
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-07-02 11:06:27 +0200

    Fix bug where variables remain active after scope close
    After the scope closed the current value was still present in ram. This is cleared now

--- processor/src/main/java/nl/xillio/xill/components/instructions/VariableDeclaration.java
+++ processor/src/main/java/nl/xillio/xill/components/instructions/VariableDeclaration.java
@@ -5,8 +5,8 @@ import java.util.Collection;
 
 import nl.xillio.xill.CodePosition;
 import nl.xillio.xill.api.Debugger;
-import nl.xillio.xill.api.components.InstructionFlow;
 import nl.xillio.xill.api.components.ExpressionBuilder;
+import nl.xillio.xill.api.components.InstructionFlow;
 import nl.xillio.xill.api.components.MetaExpression;
 import nl.xillio.xill.api.components.Processable;
 import nl.xillio.xill.api.errors.RobotRuntimeException;
@@ -16,65 +16,67 @@ import nl.xillio.xill.api.errors.RobotRuntimeException;
  */
 public class VariableDeclaration extends Instruction {
 
-	private final Processable expression;
-	private MetaExpression currentValue;
+    private final Processable expression;
+    private MetaExpression currentValue;
 
-	/**
-	 * Create a new {@link VariableDeclaration}
-	 *
-	 * @param expression
-	 */
-	public VariableDeclaration(final Processable expression) {
-		this.expression = expression;
+    /**
+     * Create a new {@link VariableDeclaration}
+     *
+     * @param expression
+     */
+    public VariableDeclaration(final Processable expression) {
+	this.expression = expression;
+    }
+
+    @Override
+    public InstructionFlow<MetaExpression> process(final Debugger debugger) throws RobotRuntimeException {
+	InstructionFlow<MetaExpression> result = expression.process(debugger);
+
+	if (result.hasValue()) {
+	    currentValue = result.get();
+	} else {
+	    currentValue = ExpressionBuilder.NULL;
 	}
 
-	@Override
-	public InstructionFlow<MetaExpression> process(final Debugger debugger) throws RobotRuntimeException {
-		InstructionFlow<MetaExpression> result = expression.process(debugger);
-		
-		if(result.hasValue())
-			currentValue = result.get();
-		else
-			currentValue = ExpressionBuilder.NULL;
-		
-		return InstructionFlow.doResume();
-	}
+	return InstructionFlow.doResume();
+    }
 
-	/**
-	 * @return the expression of the variable
-	 */
-	public MetaExpression getVariable() {
-		return currentValue;
-	}
+    /**
+     * @return the expression of the variable
+     */
+    public MetaExpression getVariable() {
+	return currentValue;
+    }
 
-	/**
-	 * Set the internal value of the variable
-	 *
-	 * @param value
-	 */
-	public void setVariable(final MetaExpression value) {
-		currentValue = value;
-	}
+    /**
+     * Set the internal value of the variable
+     *
+     * @param value
+     */
+    public void setVariable(final MetaExpression value) {
+	currentValue = value;
+    }
 
-	/**
-	 * @param position 
-	 * @return A declaration with value {@link ExpressionBuilder#NULL}
-	 */
-	public static VariableDeclaration nullDeclaration(final CodePosition position) {
-		VariableDeclaration dec = new VariableDeclaration(ExpressionBuilder.NULL);
-		dec.setPosition(position);
+    /**
+     * @param position
+     * @return A declaration with value {@link ExpressionBuilder#NULL}
+     */
+    public static VariableDeclaration nullDeclaration(final CodePosition position) {
+	VariableDeclaration dec = new VariableDeclaration(ExpressionBuilder.NULL);
+	dec.setPosition(position);
 
-		return dec;
-	}
+	return dec;
+    }
 
-	@Override
-	public Collection<Processable> getChildren() {
-		return Arrays.asList(expression);
-	}
+    @Override
+    public Collection<Processable> getChildren() {
+	return Arrays.asList(expression);
+    }
 
-	@Override
-	public void close() throws Exception {
-		currentValue.close();
-	}
+    @Override
+    public void close() throws Exception {
+	currentValue.close();
+	currentValue = null;
+    }
 
 }
