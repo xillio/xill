7ad75cb60f6da67656a9e1d268396088185ef14b
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-07-02 15:40:19 +0200

    Fix issue with circular dependencies in robots
    When robots had circular dependencies disposing them would cause an infinite loop.

--- processor/src/main/java/nl/xillio/xill/components/Robot.java
+++ processor/src/main/java/nl/xillio/xill/components/Robot.java
@@ -26,72 +26,81 @@ import nl.xillio.xill.components.instructions.InstructionSet;
  * This class represents the root node of the program structure
  */
 public class Robot extends InstructionSet implements nl.xillio.xill.api.components.Robot {
-	private final RobotID robotID;
-	private final List<nl.xillio.xill.api.components.Robot> libraries = new ArrayList<>();
-	private MetaExpression callArgument = ExpressionBuilder.NULL;
+    private final RobotID robotID;
+    private final List<nl.xillio.xill.api.components.Robot> libraries = new ArrayList<>();
+    private MetaExpression callArgument = ExpressionBuilder.NULL;
+    private final static List<nl.xillio.xill.api.components.Robot> initializingRobots = new ArrayList<>();
+    private final static List<nl.xillio.xill.api.components.Robot> closingRobots = new ArrayList<>();
 
-	/**
-	 * @param robotID
-	 * @param debugger
-	 */
-	public Robot(final RobotID robotID, final Debugger debugger) {
+    /**
+     * @param robotID
+     * @param debugger
+     */
+    public Robot(final RobotID robotID, final Debugger debugger) {
 	super(debugger);
 	this.robotID = robotID;
-	}
+    }
 
-	/**
-	 * Process this robot if necessary
-	 *
-	 * @return the result
-	 */
-	@Override
-	public InstructionFlow<MetaExpression> process(final Debugger debugger) throws RobotRuntimeException {
+    /**
+     * Process this robot if necessary
+     *
+     * @return the result
+     */
+    @Override
+    public InstructionFlow<MetaExpression> process(final Debugger debugger) throws RobotRuntimeException {
 	getDebugger().robotStarted(this);
 
+	initializingRobots.add(this);
 	for (nl.xillio.xill.api.components.Robot robot : libraries) {
-		robot.initialize();
+	    robot.initialize();
 	}
+	initializingRobots.remove(this);
 
 	InstructionFlow<MetaExpression> result = super.process(debugger);
 
 	getDebugger().robotFinished(this);
 
 	return result;
-	}
+    }
 
-	/**
-	 * @return the robotID
-	 */
-	public RobotID getRobotID() {
+    /**
+     * @return the robotID
+     */
+    public RobotID getRobotID() {
 	return robotID;
-	}
+    }
 
-	@Override
-	public Collection<Processable> getChildren() {
+    @Override
+    public Collection<Processable> getChildren() {
 	List<Processable> children = new ArrayList<>(super.getChildren());
 
 	children.addAll(libraries);
 
 	return children;
-	}
+    }
 
-	@Override
-	public void initialize() throws RobotRuntimeException {
+    @Override
+    public void initialize() throws RobotRuntimeException {
 	for (nl.xillio.xill.api.components.Robot robot : libraries) {
+	    if (!initializingRobots.contains(robot)) {
+		initializingRobots.add(robot);
 		robot.initialize();
+		initializingRobots.remove(robot);
+	    }
 	}
 
 	super.initialize();
-	}
+    }
 
-	/**
-	 * Use a BFS algorithm to find a target among the children
-	 * 
-	 * @param target
-	 * @return the path to the target or an empty list if the target wasn't found.
-	 */
-	@Override
-	public List<Processable> pathToInstruction(final Instruction target) {
+    /**
+     * Use a BFS algorithm to find a target among the children
+     * 
+     * @param target
+     * @return the path to the target or an empty list if the target wasn't
+     *         found.
+     */
+    @Override
+    public List<Processable> pathToInstruction(final Instruction target) {
 	Queue<Processable> fringe = new LinkedList<>();
 	Set<Processable> visited = new HashSet<>();
 	Map<Processable, Processable> parents = new HashMap<>();
@@ -99,63 +108,70 @@ public class Robot extends InstructionSet implements nl.xillio.xill.api.componen
 	visited.add(this);
 	fringe.add(this);
 	while (!fringe.isEmpty()) {
-		Processable currentItem = fringe.poll();
-		visited.add(currentItem);
+	    Processable currentItem = fringe.poll();
+	    visited.add(currentItem);
 
-		if (currentItem == target) {
+	    if (currentItem == target) {
 		// Found the target, let's make the list
 		List<Processable> result = new ArrayList<>();
 
 		while (currentItem != null) {
-			result.add(currentItem);
-			currentItem = parents.get(currentItem);
+		    result.add(currentItem);
+		    currentItem = parents.get(currentItem);
 		}
 
 		return Lists.reverse(result);
-		}
+	    }
 
-		Processable parent = currentItem;
-		// Seach children
-		currentItem.getChildren().stream().filter(child -> !visited.contains(child)).forEach(child -> {
+	    Processable parent = currentItem;
+	    // Seach children
+	    currentItem.getChildren().stream().filter(child -> !visited.contains(child)).forEach(child -> {
 		visited.add(child);
 		fringe.add(child);
 		parents.put(child, parent);
-		});
+	    });
 	}
 
 	return new ArrayList<>();
-	}
+    }
 
-	@Override
-	public void close() throws Exception {
+    @Override
+    public void close() throws Exception {
 	super.close();
 
+	if(closingRobots.contains(this)) {
+	    return;
+	}
+	
+	closingRobots.add(this);
 	// Close all external robots
 	for (nl.xillio.xill.api.components.Robot robot : libraries) {
-		try {
+	    try {
 		robot.close();
-		} catch (Exception e) {
+	    } catch (Exception e) {
 		e.printStackTrace();
-		}
+	    }
 	}
-	}
+	
+	closingRobots.remove(this);
+    }
 
-	/**
-	 * Add a library to this robot
-	 * 
-	 * @param lib
-	 */
-	public void addLibrary(final Robot lib) {
+    /**
+     * Add a library to this robot
+     * 
+     * @param lib
+     */
+    public void addLibrary(final Robot lib) {
 	libraries.add(lib);
-	}
+    }
 
-	@Override
-	public void setArgument(final MetaExpression expression) {
+    @Override
+    public void setArgument(final MetaExpression expression) {
 	callArgument = expression;
-	}
+    }
 
-	@Override
-	public MetaExpression getArgument() {
+    @Override
+    public MetaExpression getArgument() {
 	return callArgument;
-	}
+    }
 }
