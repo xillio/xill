246cc079338e25f02efdc7a5ceac7ada7ee76938
Ivor van der Hoog <vanderhoog@gmail.com>
2015-07-14 12:17:45 +0200

    Deleted obsolete files and made it so the packageHTML gets initiated correctly

--- ide/src/main/java/nl/xillio/migrationtool/documentation/DocumentSearcher.java
+++ ide/src/main/java/nl/xillio/migrationtool/documentation/DocumentSearcher.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import javafx.util.Pair;
+
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
@@ -20,26 +22,20 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 
-import javafx.util.Pair;
-
 /**
  * <p>
- * This class handles indexing a FunctionDocument and querying the database of
- * FunctionDocuments.
+ * This class handles indexing a FunctionDocument and querying the database of FunctionDocuments.
  * </p>
  * <p>
- * The public search function receives a string as query, splits that string and
- * does a fuzzy search on each of the components. It returns an array with the
- * unique ID's of functions that match that query.
+ * The public search function receives a string as query, splits that string and does a fuzzy search on each of the components. It returns an array with the unique ID's of functions that match that
+ * query.
  * </p>
  *
  * <p>
- * The public index function recieves a FunctionDocument and adds it to the
- * database.
+ * The public index function recieves a FunctionDocument and adds it to the database.
  * </p>
  * <p>
- * It returns an IndexResponse which is an object from elasticsearch, usually
- * this is cast into the void.
+ * It returns an IndexResponse which is an object from elasticsearch, usually this is cast into the void.
  * </p>
  * =======
  *
@@ -62,146 +58,166 @@ import javafx.util.Pair;
  */
 public class DocumentSearcher {
 
-    private static final String DOCUMENTATION_INDEX = "functiondocumentation";
-    private final Client client;
+	private static final String DOCUMENTATION_INDEX = "functiondocumentation";
+	private final Client client;
 
-    /**
-     * Initialises the {@link DocumentSearcher}
-     *
-     * @param c
-     *            The client the {@link DocumentSearcher} works with
-     */
-    public DocumentSearcher(final Client c) {
-	client = c;
-    }
-
-    /**
-     * <p>
-     * We search the database given a query.
-     * </p>
-     *
-     * <p>
-     * The query (a string) is split on whitespace and each word is queried in
-     * the following fasion:
-     * </p>
-     *
-     * <p>
-     * foreach( word in query):
-     * </p>
-     * <ul>
-     * <li>We fuzzyQuery the name, description, examples and searchTags</li>
-     * <li>We wildcardQuery (substring match) name and searchtags</li>
-     * <li>We boost the name and the searchtag category.</li>
-     * </ul>
-     *
-     * @param query
-     *            The search query
-     * @return An array of unique ID's of functions that match that query.
-     */
-    public String[] search(final String query) {
-	checkIndex();
-
-	BoolQueryBuilder question = QueryBuilders.boolQuery();
-	// Setup the query
-	String[] queries = query.split(" ");
-	for (String q : queries) {
-	    question = question.should(QueryBuilders.fuzzyQuery("name", q).fuzziness(Fuzziness.TWO).boost(3.0f))
-		    .should(QueryBuilders.wildcardQuery("name", "*" + q + "*").boost(3.0f))
-		    .should(QueryBuilders.fuzzyQuery("description", q)).should(QueryBuilders.fuzzyQuery("examples", q))
-		    .should(QueryBuilders.fuzzyQuery("searchTags", q).boost(3.0f))
-		    .should(QueryBuilders.wildcardQuery("searchTags", "*" + q + "*").boost(3.0f));
-	}
-	// Retrieve a response
-	SearchResponse response = client.prepareSearch(DOCUMENTATION_INDEX).setQuery(question).execute().actionGet();
-
-	// Return the ID of each response (functionname)
-	SearchHit[] hits = response.getHits().getHits();
-	String[] results = new String[hits.length];
-	for (int t = 0; t < hits.length; ++t) {
-	    results[t] = hits[t].getType() + "." + hits[t].id();
-	}
-	return results;
-    }
-
-    /**
-     * Returns a string which is the documentversion given a package and ID.
-     *
-     * @param packet
-     *            The package the function is in
-     * @param id
-     *            The unique id of the functiondocument
-     * @return The version or null when the function is non existant
-     */
-    public String getDocumentVersion(final String packet, final String id) {
-	checkIndex();
-	try {
-	    GetResponse Response = client.prepareGet(DOCUMENTATION_INDEX, packet, id).setFields("version").execute()
-		    .actionGet();
-	    return (String) Response.getField("version").getValue();
-	} catch (Exception e) {
-	    return null;
+	/**
+	 * Initialises the {@link DocumentSearcher}
+	 *
+	 * @param c
+	 *        The client the {@link DocumentSearcher} works with
+	 */
+	public DocumentSearcher(final Client c) {
+		client = c;
 	}
 
-    }
+	/**
+	 * <p>
+	 * We search the database given a query.
+	 * </p>
+	 *
+	 * <p>
+	 * The query (a string) is split on whitespace and each word is queried in the following fasion:
+	 * </p>
+	 *
+	 * <p>
+	 * foreach( word in query):
+	 * </p>
+	 * <ul>
+	 * <li>We fuzzyQuery the name, description, examples and searchTags</li>
+	 * <li>We wildcardQuery (substring match) name and searchtags</li>
+	 * <li>We boost the name and the searchtag category.</li>
+	 * </ul>
+	 *
+	 * @param query
+	 *        The search query
+	 * @return An array of unique ID's of functions that match that query.
+	 */
+	public String[] search(final String query) {
+		checkIndex();
 
-    /**
-     * Indexes a {@link FunctionDocument} into the database.
-     *
-     * @param document
-     *            The document we want to index
-     * @return An index response of the function.
-     * @throws ElasticsearchException
-     *             Throws an elasticsearchException when we get an error
-     *             indexing the {@link FunctionDocument}
-     * @throws IOException
-     *             Throws an IOException when trying to write to a file.
-     */
-    public IndexResponse index(final FunctionDocument document) throws ElasticsearchException, IOException {
-	// Create an array of the example texts
-	List<Pair<String, String>> items = new ArrayList<>(document.getExamples());
-	String[] examples = new String[items.size()];
-	for (int t = 0; t < items.size(); t++) {
-	    examples[t] = items.get(t).getValue();
+		BoolQueryBuilder question = QueryBuilders.boolQuery();
+		// Setup the query
+		String[] queries = query.split(" ");
+		for (String q : queries) {
+			question = question.should(QueryBuilders.fuzzyQuery("name", q).fuzziness(Fuzziness.TWO).boost(3.0f))
+				.should(QueryBuilders.wildcardQuery("name", "*" + q + "*").boost(3.0f))
+				.should(QueryBuilders.fuzzyQuery("description", q)).should(QueryBuilders.fuzzyQuery("examples", q))
+				.should(QueryBuilders.fuzzyQuery("searchTags", q).boost(3.0f))
+				.should(QueryBuilders.wildcardQuery("searchTags", "*" + q + "*").boost(3.0f));
+		}
+		// Retrieve a response
+		SearchResponse response = client.prepareSearch(DOCUMENTATION_INDEX).setQuery(question).execute().actionGet();
+
+		// Return the ID of each response (functionname)
+		SearchHit[] hits = response.getHits().getHits();
+		String[] results = new String[hits.length];
+		for (int t = 0; t < hits.length; ++t) {
+			results[t] = hits[t].getType() + "." + hits[t].id();
+		}
+		return results;
 	}
 
-	// Create an array with all the parameters
-	// To be adjusted in 3.0
-	List<Pair<String, String>> parameters = new ArrayList<>(document.getParameters());
-	String[] parameterstrings = new String[parameters.size()];
-	for (int t = 0; t < parameters.size(); t++) {
-	    parameterstrings[t] = parameters.get(t).getKey() + " " + parameters.get(t).getValue();
+	/**
+	 * Returns a string which is the documentversion given a package and ID.
+	 *
+	 * @param packet
+	 *        The package the function is in
+	 * @param id
+	 *        The unique id of the functiondocument
+	 * @return The version or null when the function is non existant
+	 */
+	public String getDocumentVersion(final String packet, final String id) {
+		checkIndex();
+		try {
+			GetResponse Response = client.prepareGet(DOCUMENTATION_INDEX, packet, id).setFields("version").execute()
+				.actionGet();
+			return (String) Response.getField("version").getValue();
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+	
+	/**
+	 * Returns a string which is the documentDescription given a package and ID.
+	 *
+	 * @param packet
+	 *        The package the function is in
+	 * @param id
+	 *        The unique id of the functiondocument
+	 * @return The description of the functiondocument or null when it does not exist.
+	 */
+	public String getDocumentDescription(final String packet, final String id) {
+		checkIndex();
+		try {
+			GetResponse Response = client.prepareGet(DOCUMENTATION_INDEX, packet, id).setFields("description").execute()
+				.actionGet();
+			return (String) Response.getField("version").getValue();
+		} catch (Exception e) {
+			return null;
+		}
+
 	}
 
-	// Return an indexed client with three fields
-	return client.prepareIndex(DOCUMENTATION_INDEX, document.getPackage(), document.getName())
-		.setSource(jsonBuilder().startObject().field("name", document.getName())
-			.field("description", document.getDescription()).field("parameters", parameterstrings)
-			.field("searchTags", document.getSearchTags()).field("version", document.getVersion())
-			.endObject())
+	/**
+	 * Indexes a {@link FunctionDocument} into the database.
+	 *
+	 * @param document
+	 *        The document we want to index
+	 * @return An index response of the function.
+	 * @throws ElasticsearchException
+	 *         Throws an elasticsearchException when we get an error
+	 *         indexing the {@link FunctionDocument}
+	 * @throws IOException
+	 *         Throws an IOException when trying to write to a file.
+	 */
+	public IndexResponse index(final FunctionDocument document) throws ElasticsearchException, IOException {
+		// Create an array of the example texts
+		List<Pair<String, String>> items = new ArrayList<>(document.getExamples());
+		String[] examples = new String[items.size()];
+		for (int t = 0; t < items.size(); t++) {
+			examples[t] = items.get(t).getValue();
+		}
 
-	.execute().actionGet();
-    }
+		// Create an array with all the parameters
+		// To be adjusted in 3.0
+		List<Pair<String, String>> parameters = new ArrayList<>(document.getParameters());
+		String[] parameterstrings = new String[parameters.size()];
+		for (int t = 0; t < parameters.size(); t++) {
+			parameterstrings[t] = parameters.get(t).getKey() + " " + parameters.get(t).getValue();
+		}
 
-    /**
-     * Check if the index exists, if not then create it
-     */
-    private void checkIndex() {
-	boolean indexFound = false;
+		// Return an indexed client with three fields
+		return client.prepareIndex(DOCUMENTATION_INDEX, document.getPackage(), document.getName())
+			.setSource(jsonBuilder().startObject().field("name", document.getName())
+				.field("description", document.getDescription()).field("parameters", parameterstrings)
+				.field("searchTags", document.getSearchTags()).field("version", document.getVersion())
+				.endObject())
 
-	try {
-	    IndicesExistsResponse result = client.admin().indices()
-		    .exists(new IndicesExistsRequest(DOCUMENTATION_INDEX)).get();
-	    indexFound = result.isExists();
-	} catch (InterruptedException | ExecutionException e) {
-	    e.printStackTrace();
+			.execute().actionGet();
 	}
 
-	if (!indexFound) {
-	    try {
-		client.admin().indices().create(new CreateIndexRequest(DOCUMENTATION_INDEX)).get();
-	    } catch (InterruptedException | ExecutionException e) {
-		e.printStackTrace();
-	    }
+	/**
+	 * Check if the index exists, if not then create it
+	 */
+	private void checkIndex() {
+		boolean indexFound = false;
+
+		try {
+			IndicesExistsResponse result = client.admin().indices()
+				.exists(new IndicesExistsRequest(DOCUMENTATION_INDEX)).get();
+			indexFound = result.isExists();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		if (!indexFound) {
+			try {
+				client.admin().indices().create(new CreateIndexRequest(DOCUMENTATION_INDEX)).get();
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-    }
 }
---
+++ ide/src/main/java/nl/xillio/migrationtool/documentation/FunctionIndex.java
@@ -0,0 +1,67 @@
+package nl.xillio.migrationtool.documentation;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.rendersnake.HtmlCanvas;
+
+/**
+ * @author Ivor
+ *
+ */
+public class FunctionIndex extends HtmlGenerator {
+	Set<PackageDocument> packages = new HashSet<>();
+
+	/**
+	 * The constructor for the {@link FunctionIndex}
+	 * @param name
+	 * 					The name of the {@link FunctionIndex}
+	 */
+	public FunctionIndex(String name) {
+		this.setName(name);
+	}
+
+	@Override
+	public String toHTML() throws IOException {
+		HtmlCanvas html = new HtmlCanvas();
+
+		html = addHeader(html);
+		html.body();
+		html = addTitle(html);
+
+		html = openTable(html);
+		html = addTableWithPackages(html);
+		html = closeTable(html);
+
+		html._body();
+		return html.toHtml();
+	}
+	
+	/**
+	 * <p>Adds a package to the list of preset packages through trying to remove it and then adding it again. </p>
+	 * <p> This ensures that versions are kept up to date </p>
+	 * @param packet
+	 * 						The package we're adding.
+	 */
+	public void addPackage(PackageDocument packet){
+		//Tries to remove the old package and adds this one.
+		packages.remove(packet);
+		packages.add((packet));
+	}
+	
+  /**
+   * Adds a table with all the functions in the Package and a link to them.
+   *
+   * @param canvas
+   *            The canvas we're adding the link to.
+   * @return Returns a {@link HtmlCanvas} with a section containing a table.
+   * @throws IOException
+   *             Throws an IOException when an error is encountered when
+   *             generating the HTML
+   */
+  private HtmlCanvas addTableWithPackages(HtmlCanvas canvas) throws IOException {
+  	return null;
+	  }
+
+}
