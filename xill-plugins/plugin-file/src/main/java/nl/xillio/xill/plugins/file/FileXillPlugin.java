3e788422a5b3d9c90c22ffa27cd0c6c13d021fb1
sanvisser <sanvisser@gmail.com>
2015-07-27 10:31:20 +0200

    Started file plugin
    no construct made yet

---
+++ xill-plugins/plugin-file/src/main/java/nl/xillio/xill/plugins/file/FileXillPlugin.java
@@ -0,0 +1,22 @@
+package nl.xillio.xill.plugins.file;
+
+import nl.xillio.plugins.XillPlugin;
+
+/**
+ * This package includes all example constructs
+ */
+public class FileXillPlugin extends XillPlugin {
+
+	@Override
+	public String getName() {
+		return "List";
+	}
+
+	@Override
+	public void loadConstructs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ xill-plugins/plugin-file/src/main/java/nl/xillio/xill/plugins/file/constructs/WebPreviewConstruct.java
@@ -0,0 +1,60 @@
+package nl.xillio.xill.plugins.file.constructs;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import nl.xillio.xill.api.components.AtomicExpression;
+import nl.xillio.xill.api.construct.Argument;
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+import nl.xillio.xill.api.preview.PreviewComponent;
+
+/**
+ * This construct shows how to make a custom preview
+ */
+public class WebPreviewConstruct extends Construct {
+
+    @Override
+    public String getName() {
+	return "webPreview";
+    }
+
+    @Override
+    public ConstructProcessor prepareProcess(final ConstructContext context) {
+	// To show a custom preview of the variable you need to implement the
+	// PreviewComponent interface
+
+	return new ConstructProcessor((url) -> new WebExpression(url.getStringValue()), new Argument("url"));
+    }
+
+    /**
+     * This class is an implementation of the PreviewComponent
+     */
+    private class WebExpression extends AtomicExpression implements PreviewComponent {
+
+	public WebExpression(final String url) {
+	    super(url);
+	}
+
+	@Override
+	public Node getPreview() {
+	    // Because we can't display a whole webpage in the tree structure we
+	    // can make a button that shows it when it is clicked
+
+	    Button button = new Button("View Page");
+	    button.setOnAction(action -> {
+		WebView webview = new WebView();
+		webview.getEngine().load(getStringValue());
+		Stage stage = new Stage();
+		stage.setScene(new Scene(webview));
+		stage.showAndWait();
+		stage.close();
+	    });
+
+	    return button;
+	}
+    }
+}
