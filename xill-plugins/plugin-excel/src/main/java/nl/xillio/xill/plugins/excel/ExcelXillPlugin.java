a7cbff0fc7e20886a26ba9a69885c8697198b0f7
Daan Knoope <daanknoope@outlook.com>
2015-08-04 14:45:35 +0200

    Add global structure for excel plugin
    CTC-673

---
+++ xill-plugins/plugin-excel/src/main/java/nl/xillio/xill/plugins/excel/ExcelXillPlugin.java
@@ -0,0 +1,10 @@
+package nl.xillio.xill.plugins.excel;
+
+import nl.xillio.plugins.XillPlugin;
+
+/**
+ * This package includes all example constructs
+ */
+public class ExcelXillPlugin extends XillPlugin {
+
+}
---
+++ xill-plugins/plugin-excel/src/test/java/nl/xillio/xill/plugins/example/LifeConstructTest.java
@@ -0,0 +1,36 @@
+package nl.xillio.xill.plugins.example;
+
+import static org.mockito.Mockito.mock;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+import nl.xillio.xill.plugins.example.constructs.LifeConstuct;
+
+/**
+ * Test the {@link LifeConstuct}
+ */
+public class LifeConstructTest {
+
+	/**
+	 * Test the construct under normal usage
+	 */
+	@Test
+	public void testPrepareProcess() {
+		// We do not want to test the ConstructContext so we mock this
+		ConstructContext context = mock(ConstructContext.class);
+
+		// To test a construct we need to prepare a process from it.
+		ConstructProcessor processor = new LifeConstuct().prepareProcess(context);
+
+		// Get the result from the processor
+		MetaExpression result = processor.process();
+
+		// Make assertions
+		Assert.assertEquals(result.getNumberValue().intValue(), 42);
+		Assert.assertEquals(processor.getNumberOfArguments(), 0);
+	}
+}
