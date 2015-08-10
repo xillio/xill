2c0074f654200ad4ad1333856f8fd02700e549d4
Geert Konijnendijk <geert@konijnendijk.info>
2015-08-10 13:12:06 +0200

    Add Database Plugin

---
+++ xill-plugins/plugin-database/src/main/java/nl/xillio/xill/plugins/database/DatabaseXillPlugin.java
@@ -0,0 +1,10 @@
+package nl.xillio.xill.plugins.database;
+
+import nl.xillio.plugins.XillPlugin;
+
+/**
+ * This package includes all Database constructs
+ */
+public class DatabaseXillPlugin extends XillPlugin {
+
+}
