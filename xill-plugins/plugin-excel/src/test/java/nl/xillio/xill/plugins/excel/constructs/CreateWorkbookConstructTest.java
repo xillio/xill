4bd6e82eb0cc115a6dd427f16151d9eb8f91bc38
Daan Knoope <daanknoope@outlook.com>
2015-08-11 16:01:38 +0200

    Added unit test for CreateWorkbookConstruct
    Added three unit test for the createWorkbookConstruct.
    
    CTC-673

---
+++ xill-plugins/plugin-excel/src/test/java/nl/xillio/xill/plugins/excel/constructs/CreateWorkbookConstructTest.java
@@ -0,0 +1,68 @@
+package nl.xillio.xill.plugins.excel.constructs;
+
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.components.RobotID;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.errors.RobotRuntimeException;
+import nl.xillio.xill.plugins.excel.dataStructures.XillWorkbook;
+import nl.xillio.xill.plugins.excel.services.ExcelService;
+import nl.xillio.xill.services.inject.InjectorUtils;
+import org.apache.logging.log4j.core.util.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.rmi.UnexpectedException;
+
+import static nl.xillio.xill.api.construct.ExpressionBuilderHelper.fromValue;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+/**
+ * Created by Daan Knoope on 11-8-2015.
+ */
+public class CreateWorkbookConstructTest {
+
+	@Test (expectedExceptions = RobotRuntimeException.class, expectedExceptionsMessageRegExp = "File already exists: no new workbook has been created.")
+	public void testProcessFileAlreadyExists() throws Exception {
+		InjectorUtils.getGlobalInjector();
+		File file = mock(File.class);
+		ExcelService service = mock(ExcelService.class);
+		ConstructContext context = mock(ConstructContext.class);
+		RobotID id = mock(RobotID.class);
+		when(id.getPath()).thenReturn(new File("."));
+		when(context.getRobotID()).thenReturn(id);
+		when(service.createWorkbook(any(File.class))).thenThrow(new FileAlreadyExistsException("File already exists: no new workbook has been created."));
+		CreateWorkbookConstruct.process(service, context, fromValue("."));
+	}
+
+	@Test(expectedExceptions = RobotRuntimeException.class, expectedExceptionsMessageRegExp = "Cannot write to the supplied path")
+	public void testProcessCannotWriteToPath() throws Exception{
+		InjectorUtils.getGlobalInjector();
+		File file = mock(File.class);
+		ExcelService service = mock(ExcelService.class);
+		ConstructContext context = mock(ConstructContext.class);
+		RobotID id = mock(RobotID.class);
+		when(id.getPath()).thenReturn(new File("."));
+		when(context.getRobotID()).thenReturn(id);
+		when(service.createWorkbook(any(File.class))).thenThrow(new IOException());
+		CreateWorkbookConstruct.process(service, context, fromValue("."));
+	}
+
+	@Test
+	public void testProcessContainsWorkbookInMeta() throws Exception{
+		InjectorUtils.getGlobalInjector();
+		File file = mock(File.class);
+		ExcelService service = mock(ExcelService.class);
+		ConstructContext context = mock(ConstructContext.class);
+		XillWorkbook workbook = mock(XillWorkbook.class);
+		RobotID id = mock(RobotID.class);
+		when(id.getPath()).thenReturn(new File("."));
+		when(context.getRobotID()).thenReturn(id);
+		when(service.createWorkbook(any(File.class))).thenReturn(workbook);
+		when(workbook.getFileString()).thenReturn("string");
+		MetaExpression returned = CreateWorkbookConstruct.process(service,context,fromValue("."));
+		assertEquals(returned.getMeta(XillWorkbook.class), workbook);
+	}
+}
