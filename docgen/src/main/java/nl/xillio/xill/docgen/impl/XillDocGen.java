9f1aa0f09e9f17a9ac9196ff42bf90c200591989
Ivor van der Hoog <vanderhoog@gmail.com>
2015-08-12 17:07:51 +0200

    Made it so the docgen has a client

--- docgen/src/main/java/nl/xillio/xill/docgen/impl/XillDocGen.java
+++ docgen/src/main/java/nl/xillio/xill/docgen/impl/XillDocGen.java
@@ -5,32 +5,35 @@ import nl.xillio.xill.docgen.DocumentationGenerator;
 import nl.xillio.xill.docgen.DocumentationParser;
 import nl.xillio.xill.docgen.DocumentationSearcher;
 
+import org.elasticsearch.client.Client;
+
 /**
  * This {@link DocGen} uses a template system and a elasticsearch backed searcher
+ * 
  * @author Thomas Biesaart
  * @author Ivor van der Hoog
  * @since 12-8-2015
  */
 public class XillDocGen implements DocGen {
+	Client client;
 
-    public XillDocGen() {
-        //TODO Build elasticsearch node
-    }
+	public XillDocGen(final Client client) {
+		this.client = client;
+	}
 
-    @Override
-    public DocumentationParser getParser() {
-        return new XmlDocumentationParser();
-    }
+	@Override
+	public DocumentationParser getParser() {
+		return new XmlDocumentationParser();
+	}
 
-    @Override
-    public DocumentationGenerator getGenerator(String collectionIdentity) {
-        return new FreeMarkerDocumentationGenerator(collectionIdentity);
-    }
+	@Override
+	public DocumentationGenerator getGenerator(final String collectionIdentity) {
+		return new FreeMarkerDocumentationGenerator(collectionIdentity);
+	}
 
-    @Override
-    public DocumentationSearcher getSearcher() {
-        return new ElasticsearchDocumentationSearcher();
-    }
-
+	@Override
+	public DocumentationSearcher getSearcher() {
+		return new ElasticsearchDocumentationSearcher(client);
+	}
 
 }
