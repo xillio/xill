cd17e23ab7e2231b5c160fbcc0a860bb7e9466de
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-08-12 13:10:04 +0200

    Add methods to DocumentationEntity interface

--- docgen/src/main/java/nl/xillio/xill/docgen/DocumentationEntity.java
+++ docgen/src/main/java/nl/xillio/xill/docgen/DocumentationEntity.java
@@ -1,6 +1,5 @@
 package nl.xillio.xill.docgen;
 
-import java.util.Map;
 
 /**
  * This interface represents a piece of documentation of xill constructs
@@ -9,5 +8,15 @@ import java.util.Map;
  * @since 12-8-2015
  */
 public interface DocumentationEntity extends PropertiesProvider {
+    /**
+     * Get the identity of this name
+     * @return a single word identity that represents this entry. This would generally be a document name.
+     */
     String getIdentity();
+
+    /**
+     * Get the type of entity
+     * @return a single word identifier that represents the entity type
+     */
+    String getType();
 }
--- docgen/src/main/java/nl/xillio/xill/docgen/DocumentationGenerator.java
+++ docgen/src/main/java/nl/xillio/xill/docgen/DocumentationGenerator.java
@@ -2,6 +2,8 @@ package nl.xillio.xill.docgen;
 
 import nl.xillio.xill.docgen.exceptions.ParsingException;
 
+import java.util.IllegalFormatException;
+
 /**
  * This interface represents an object that can generate output from a {@link DocumentationEntity}
  * @author Thomas Biesaart
@@ -9,5 +11,11 @@ import nl.xillio.xill.docgen.exceptions.ParsingException;
  * @since 12-8-2015
  */
 public interface DocumentationGenerator extends AutoCloseable {
-    void generate(DocumentationEntity entity) throws ParsingException;
+    /**
+     * Generate a documentation file from a {@link DocumentationEntity}
+     * @param entity
+     * @throws ParsingException if parsing the entity failed
+     * @throws IllegalStateException if this DocumentationGenerator has already been closed
+     */
+    void generate(DocumentationEntity entity) throws ParsingException, IllegalFormatException;
 }
