a0a94df32798828a6eb48ff8d2d02540abe9eec6
Geert Konijnendijk <geert@konijnendijk.info>
2015-08-25 15:25:23 +0200

    Write test for SQLite database service

---
+++ xill-plugins/plugin-database/src/test/java/nl/xillio/xill/plugins/database/services/SQLiteDatabaseServiceImplTest.java
@@ -0,0 +1,93 @@
+package nl.xillio.xill.plugins.database.services;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import nl.xillio.xill.plugins.database.util.Tuple;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Tests the SQLite specific database service
+ * 
+ * @author Geert Konijnendijk
+ *
+ */
+public class SQLiteDatabaseServiceImplTest {
+
+	private SQLiteDatabaseServiceImpl service;
+
+	@BeforeClass
+	public void setup() {
+		service = new SQLiteDatabaseServiceImpl();
+	}
+
+	/**
+	 * Test the createConnection method
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void testCreateConnection() throws SQLException {
+		// Mock
+		Connection con = mock(Connection.class);
+		String connectionURL = "URL";
+		Tuple[] options = new Tuple[] {};
+		SQLiteDatabaseServiceImpl spyService = spy(service);
+		doReturn(con).when(spyService).connect(any());
+		doReturn(connectionURL).when(spyService).createConnectionURL(notNull(String.class), any(), any());
+
+		// Run
+		Connection returnedCon = spyService.createConnection("db", null, null, options);
+
+		// Verify
+		verify(spyService).createConnectionURL(notNull(String.class), isNull(String.class), isNull(String.class));
+		verify(spyService).connect(connectionURL);
+
+		// Assert
+		assertSame(returnedCon, con, "Different connection was returned");
+	}
+
+	/**
+	 * Test the creation of a URL for an in-memory database
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void testCreateConnectionURLMemory() throws SQLException {
+		String database = null;
+		testCreateConnectionURL(database, "jdbc:sqlite::memory:");
+	}
+
+	/**
+	 * Test the creation of a URL for a file database
+	 * 
+	 * @throws SQLException
+	 */
+	@Test
+	public void testCreateConnectionURLFile() throws SQLException {
+		String database = "test.sqlite";
+		testCreateConnectionURL(database, "jdbc:sqlite:test.sqlite");
+	}
+
+	private void testCreateConnectionURL(String database, String expectedURL) throws SQLException {
+		// Run
+		String URL = service.createConnectionURL(database, null, null, null);
+
+		// Verify
+
+		// Assert
+		assertEquals(URL, expectedURL, "Incorrect URL created");
+	}
+}
