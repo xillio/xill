07ea56aa941eb802a6d95d5740446f99f7660180
Daan Knoope <daanknoope@outlook.com>
2015-08-27 13:30:38 +0200

    Add unit tests for Tuple util
    CTC-671

---
+++ xill-plugins/plugin-database/src/test/java/nl/xillio/xill/plugins/database/util/TupleTest.java
@@ -0,0 +1,24 @@
+package nl.xillio.xill.plugins.database.util;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Unit tests for the {@link Tuple} utility
+ * @author Daan Knoope
+ */
+public class TupleTest {
+
+	/**
+	 * Tests creation and properties of the tuple
+	 * @throws Exception
+	 */
+	@Test
+	public void tupleTest() throws Exception{
+		Tuple<Integer, String> tuple = new Tuple<>(1, "value");
+		assertEquals(tuple.getValue(), "value");
+		assertEquals((int)tuple.getKey(), 1);
+	}
+
+}
