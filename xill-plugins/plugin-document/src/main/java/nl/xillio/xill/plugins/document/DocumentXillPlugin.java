98d8e377a26d9a3c973b5a031f08871cf4facb31
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-09-10 09:17:11 +0200

    Create empty plugin project for Document plugin
    fix CTC-814

---
+++ xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/document/DocumentXillPlugin.java
@@ -0,0 +1,11 @@
+package nl.xillio.xill.plugins.document;
+
+import nl.xillio.plugins.XillPlugin;
+
+/**
+ * This plugin package is responsible for operations regarding the Unified Data Model.
+ * @author Thomas Biesaart
+ * @since 3.0.0
+ */
+public class DocumentXillPlugin extends XillPlugin {
+}
