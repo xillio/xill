70cab63e053928c4d39046edb471c0e222c31e37
Luca Scalzotto <lucascalzotto@gmail.com>
2015-09-24 11:08:23 +0200

    Disabled drag-and-drop for the help view (including cursor graphic).

--- ide/src/main/java/nl/xillio/migrationtool/gui/HelpPane.java
+++ ide/src/main/java/nl/xillio/migrationtool/gui/HelpPane.java
@@ -2,14 +2,16 @@ package nl.xillio.migrationtool.gui;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.CookieHandler;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.input.DragEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 import nl.xillio.migrationtool.Loader;
@@ -51,6 +53,14 @@ public class HelpPane extends AnchorPane {
 
 		//Load splash page
 		webFunctionDoc.getEngine().load(getClass().getResource("/docgen/resources/splash.html").toExternalForm());
+		
+		// Disable drag-and-drop, set the cursor graphic when dragging.
+		webFunctionDoc.setOnDragDropped(null);
+		webFunctionDoc.setOnDragOver(new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				getScene().setCursor(Cursor.DISAPPEAR);
+			};
+		});
 	}
 
 	private void home() {
