558876069c57b3071c11d83e1188c52ff5d4669d
Geert Konijnendijk <geert@konijnendijk.info>
2015-09-29 13:54:48 +0200

    Start implementing conversion layer between Xill and the UDM
    Created a ConversionService and its implementation. Added UDM to the dependencies.

---
+++ xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/document/services/ConversionServiceImpl.java
@@ -0,0 +1,55 @@
+package nl.xillio.xill.plugins.document.services;
+
+import java.util.Map;
+
+import nl.xillio.udm.builders.DocumentRevisionBuilder;
+
+public class ConversionServiceImpl implements ConversionService {
+
+	// @Override
+	// public Map<String, Map<String, Object>> udmToMap(List<Decorator> decorators) {
+	// Map<String, Map<String, Object>> result = new HashMap<>();
+	//
+	// for (Decorator decorator : decorators){
+	// Map<String, Object> fields = new HashMap<>();
+	// for (Field field : decorator.getFields()){
+	// fields.put(field.getName(), field.getValue());
+	// }
+	// result.put(decorator.getName(), fields);
+	// }
+	//
+	// return result;
+	// }
+	//
+	// @Override
+	// public List<Decorator> mapToUdm(Map<String, Map<String, Object>> object) {
+	// List<Decorator> decorators = new ArrayList<>();
+	// for (Entry<String, Map<String, Object>> decoratorEntry : object.entrySet()) {
+	// Decorator decorator = new Decorator();
+	// decorator.setName(decoratorEntry.getKey());
+	// List<Field> fields = decorator.getFields();
+	// for (Entry<String, Object> fieldEntry : decoratorEntry.getValue().entrySet()) {
+	// Field field = new Field();
+	// field.setName(fieldEntry.getKey());
+	// field.setValue(fieldEntry.getValue());
+	// fields.add(field);
+	// }
+	// decorators.add(decorator);
+	// }
+	//
+	// return decorators;
+	// }
+
+	@Override
+	public void mapToUdm(Map<String, Map<String, Object>> object, DocumentRevisionBuilder builder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, Map<String, Object>> udmToMap(DocumentRevisionBuilder builder) {
+		// TODO Auto-generated method stub
+		return null;
+		}
+
+}
