0bbe0bbc840b18f9473a804d539a61b808c925bf
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-10-02 11:30:29 +0200

    Create a service every thing a get operation is called

--- xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/document/services/XillUDMServiceImpl.java
+++ xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/document/services/XillUDMServiceImpl.java
@@ -1,7 +1,6 @@
 package nl.xillio.xill.plugins.document.services;
 
-import java.util.Map;
-
+import com.google.inject.Inject;
 import nl.xillio.udm.DocumentID;
 import nl.xillio.udm.UDM;
 import nl.xillio.udm.builders.DocumentBuilder;
@@ -10,54 +9,46 @@ import nl.xillio.udm.builders.DocumentRevisionBuilder;
 import nl.xillio.udm.services.UDMService;
 import nl.xillio.xill.plugins.document.exceptions.VersionNotFoundException;
 
-import com.google.inject.Inject;
+import java.util.Map;
 
 public class XillUDMServiceImpl implements XillUDMService {
 
-	@Inject
-	private ConversionService conversionService;
-
-	// Service wide UDM Service
-	private UDMService udmService;
-
-	public XillUDMServiceImpl() {
-		// Connect to the UDM
-		udmService = UDM.connect();
-	}
+	private final ConversionService conversionService;
 
 	/**
 	 * Create a new XillUDMService and set the services it depends on manually
-	 * 
-	 * @param conversionService
-	 *        The {@link ConversionService}
-	 * @param udmService
-	 *        The {@link UDMService}
+	 *
+	 * @param conversionService The {@link ConversionService}
 	 */
-	XillUDMServiceImpl(ConversionService conversionService, UDMService udmService) {
+	@Inject
+	XillUDMServiceImpl(ConversionService conversionService) {
 		this.conversionService = conversionService;
-		this.udmService = udmService;
+	}
+
+	protected UDMService connect() {
+		return UDM.connect();
 	}
 
 	@Override
 	public Map<String, Map<String, Object>> get(String documentId, String versionId, String section) {
 		// Get the document and convert it to a map.
-		DocumentID docId = udmService.get(documentId);
-		DocumentRevisionBuilder document = getVersion(getSourceOrTarget(udmService.document(docId), section), versionId);
-		Map<String, Map<String, Object>> result = conversionService.udmToMap(document);
+		try (UDMService udmService = connect()) {
+			DocumentID docId = udmService.get(documentId);
+			DocumentRevisionBuilder document = getVersion(getSourceOrTarget(udmService.document(docId), section), versionId);
+			Map<String, Map<String, Object>> result = conversionService.udmToMap(document);
 
-		// Clear the cache.
-		udmService.release(docId);
+			// Clear the cache.
+			udmService.release(docId);
 
-		return result;
+			return result;
+		}
 	}
 
 	/**
 	 * Get the source or target from a {@link DocumentBuilder}.
-	 * 
-	 * @param builder
-	 *        The builder to get the section from.
-	 * @param section
-	 *        The section to return. Can be either "source" or "target", and is case-insensitive.
+	 *
+	 * @param builder The builder to get the section from.
+	 * @param section The section to return. Can be either "source" or "target", and is case-insensitive.
 	 * @return A {@link DocumentHistoryBuilder} of the given builder.
 	 */
 	private DocumentHistoryBuilder getSourceOrTarget(DocumentBuilder builder, String section) {
@@ -68,16 +59,14 @@ public class XillUDMServiceImpl implements XillUDMService {
 			return builder.target();
 		else
 			throw new IllegalArgumentException("The \"section\" argument is not in a valid format."
-					+ "Valid values are \"source\" and \"target\".");
+				+ "Valid values are \"source\" and \"target\".");
 	}
 
 	/**
 	 * Get a certain revision from a {@link DocumentHistoryBuilder}.
-	 * 
-	 * @param builder
-	 *        The builder to get the revision from.
-	 * @param version
-	 *        The version to het from the builder.
+	 *
+	 * @param builder The builder to get the revision from.
+	 * @param version The version to het from the builder.
 	 * @return A {@link DocumentRevisionBuilder} that is the correct version of the document.
 	 */
 	private DocumentRevisionBuilder getVersion(DocumentHistoryBuilder builder, String version) {
