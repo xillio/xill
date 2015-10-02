2c969dab00dba3cd0e0adc47950d9e65756b8098
sanvisser <sanvisser@gmail.com>
2015-10-02 11:23:33 +0200

    work on remove construct
    removing whole entries works, and versions too.. so I think it works?
    I don't have an create document so I can't test it very well.
    
    when I get the service I will split this into construct/service.

--- xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/document/constructs/RemoveConstruct.java
+++ xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/document/constructs/RemoveConstruct.java
@@ -2,13 +2,29 @@ package nl.xillio.xill.plugins.document.constructs;
 
 import com.google.inject.Inject;
 
+import nl.xillio.udm.DocumentID;
+import nl.xillio.udm.UDM;
+import nl.xillio.udm.exceptions.DocumentNotFoundException;
+import nl.xillio.udm.exceptions.PersistenceException;
+import nl.xillio.udm.services.UDMService;
 import nl.xillio.xill.api.components.MetaExpression;
 import nl.xillio.xill.api.construct.Argument;
 import nl.xillio.xill.api.construct.Construct;
 import nl.xillio.xill.api.construct.ConstructContext;
 import nl.xillio.xill.api.construct.ConstructProcessor;
-public class RemoveConstruct extends Construct {
+import nl.xillio.xill.api.errors.RobotRuntimeException;
 
+
+
+/**
+ * 
+ * Construct for removing documents or removing a version of a document.
+ * if versionid is left empty the whole document will be removed otherwise only the given version.
+ * 
+ * @author Sander Visser
+ *
+ */
+public class RemoveConstruct extends Construct {
 
 	@Override
 	public ConstructProcessor prepareProcess(ConstructContext context) {
@@ -16,7 +32,7 @@ public class RemoveConstruct extends Construct {
 				(documentid, versionid, section) -> process(documentid, versionid, section),
 				new Argument("documentid", ATOMIC),
 				new Argument("versionid",fromValue("current"), ATOMIC),
-				new Argument("section", fromValue("target"), ATOMIC)
+				new Argument("source", fromValue("target"), ATOMIC)
 		);
 	}
 
@@ -26,11 +42,48 @@ public class RemoveConstruct extends Construct {
 		String versid = versionid.getStringValue();
 		String sect = section.getStringValue();
 		
+		if(sect.equals("temp")){
+			UDMService udm = UDM.connect();
+			DocumentID doc = udm.create()
+					.contentType().name("name")
+					.source().current().version("2")
+					.source().current().action(2)
+					.target().current().action(3)
+					.target().current().version("testing")
+					.target().revision("whut")
+					.revision("whut").version("deze").action(5)
+					.revision("aarde").version("nieuwe").action(6)
+					.modifiedBy("ugh")
+					.commit();
+			
+			
+			try {
+				udm.persist(doc);
+			} catch (PersistenceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return NULL;
+		}
+		
 		//connect to UDM service
-		
-		
-
-		
+		try(UDMService udm = UDM.connect()){
+			DocumentID doc = udm.get(docid);
+			if(versid.equals("current")){
+				udm.delete(doc);
+			}else{
+				if(sect.equals("source")){
+					udm.document(doc).source().removeRevision(versid).commit();
+				}else if (sect.equals("target")){
+					udm.document(doc).target().removeRevision(versid).commit();
+				}
+				udm.persist(doc);
+			}
+			
+		}catch(PersistenceException | DocumentNotFoundException e){
+			throw new RobotRuntimeException(e.getMessage());
+			
+		}
 		return NULL;
 	}
 }
