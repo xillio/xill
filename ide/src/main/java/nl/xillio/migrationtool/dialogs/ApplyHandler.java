dcdfc2d8edd480a1186eb97bcab13a02b4f213ac
Pieter Dirk Soels <pieter.soels@xillio.nl>
2015-11-19 11:54:37 +0100

    Implemented ApplyHandler interface (Thread solving)

---
+++ ide/src/main/java/nl/xillio/migrationtool/dialogs/ApplyHandler.java
@@ -0,0 +1,12 @@
+package nl.xillio.migrationtool.dialogs;
+
+/**
+ * This interface is a blueprint for applying the updated settings
+ *
+ * Created by Pieter Soels on 19/11/2015.
+ */
+
+@FunctionalInterface
+public interface ApplyHandler {
+    void applySettings();
+}
