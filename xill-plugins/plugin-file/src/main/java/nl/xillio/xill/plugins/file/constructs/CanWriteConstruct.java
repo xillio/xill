c22e41e0a21810e63f7331fa5d1d9d27b8c0c6ec
Anwar Rahimbaks <anwar.rahimbaks@xillio.nl>
2015-11-30 17:14:19 +0100

    Added the four constructs for the file permissions.
    CTC-1115.

---
+++ xill-plugins/plugin-file/src/main/java/nl/xillio/xill/plugins/file/constructs/CanWriteConstruct.java
@@ -0,0 +1,17 @@
+package nl.xillio.xill.plugins.file.constructs;
+
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+
+/**
+ * Determines whether a file or folder is readable or not.
+ *
+ * Created by Anwar on 11/30/2015.
+ */
+public class CanWriteConstruct extends Construct {
+    @Override
+    public ConstructProcessor prepareProcess(ConstructContext context) {
+        return null;
+    }
+}
---
+++ xill-plugins/plugin-file/src/main/java/nl/xillio/xill/plugins/file/constructs/IsHiddenConstruct.java
@@ -0,0 +1,17 @@
+package nl.xillio.xill.plugins.file.constructs;
+
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+
+/**
+ * Determines whether a file or folder is hidden or not.
+ *
+ * Created by Anwar on 11/30/2015.
+ */
+public class IsHiddenConstruct extends Construct {
+    @Override
+    public ConstructProcessor prepareProcess(ConstructContext context) {
+        return null;
+    }
+}
