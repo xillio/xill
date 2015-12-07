d025287509b8e5a92375a65fd26ca18c7a9fe85f
Pieter Dirk Soels <pieter.soels@xillio.nl>
2015-12-07 22:17:32 +0100

    Fixed filter returns same MetaExpression type.

--- processor/src/main/java/nl/xillio/xill/components/expressions/FilterExpression.java
+++ processor/src/main/java/nl/xillio/xill/components/expressions/FilterExpression.java
@@ -1,10 +1,6 @@
 package nl.xillio.xill.components.expressions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 import nl.xillio.xill.api.Debugger;
@@ -12,6 +8,7 @@ import nl.xillio.xill.api.components.InstructionFlow;
 import nl.xillio.xill.api.components.MetaExpression;
 import nl.xillio.xill.api.components.Processable;
 import nl.xillio.xill.api.construct.ExpressionBuilderHelper;
+import nl.xillio.xill.api.errors.NotImplementedException;
 import nl.xillio.xill.api.errors.RobotRuntimeException;
 import nl.xillio.xill.components.instructions.FunctionDeclaration;
 
@@ -35,7 +32,8 @@ public class FilterExpression implements Processable, FunctionParameterExpressio
 	@SuppressWarnings("unchecked")
 	@Override
 	public InstructionFlow<MetaExpression> process(final Debugger debugger) throws RobotRuntimeException {
-		List<MetaExpression> results = new ArrayList<>();
+		List<MetaExpression> listResults = new ArrayList<>();
+		LinkedHashMap<String, MetaExpression> objectResults = new LinkedHashMap<>();
 
 		// Call the function for all arguments
 		for (Processable argument : arguments) {
@@ -46,18 +44,19 @@ public class FilterExpression implements Processable, FunctionParameterExpressio
 					// Process the one argument
 					MetaExpression expressionValue = functionDeclaration.run(debugger, Arrays.asList(result)).get();
 					if (expressionValue.getBooleanValue()) {
-						results.add(result);
+						return InstructionFlow.doResume(result);
+					} else {
+						return InstructionFlow.doResume(ExpressionBuilderHelper.emptyList());
 					}
-					break;
 				case LIST:
 					// Pass every argument
 					for (MetaExpression expression : (List<MetaExpression>) result.getValue()) {
 						MetaExpression value = functionDeclaration.run(debugger, Arrays.asList(expression)).get();
 						if (value.getBooleanValue()) {
-							results.add(expression);
+							listResults.add(expression);
 						}
 					}
-					break;
+					return InstructionFlow.doResume(ExpressionBuilderHelper.fromValue(listResults));
 				case OBJECT:
 					// Pass every argument but with key
 					for (Entry<String, MetaExpression> expression : ((Map<String, MetaExpression>) result.getValue())
@@ -66,17 +65,15 @@ public class FilterExpression implements Processable, FunctionParameterExpressio
 							Arrays.asList(ExpressionBuilderHelper.fromValue(expression.getKey()), expression.getValue()))
 							.get();
 						if (value.getBooleanValue()) {
-							results.add(expression.getValue());
+							objectResults.put(expression.getKey(), expression.getValue());
 						}
 					}
-					break;
+					return InstructionFlow.doResume(ExpressionBuilderHelper.fromValue(objectResults));
 				default:
-					break;
-
+					throw new NotImplementedException("This MetaExpression has not been defined yet.");
 			}
 		}
-
-		return InstructionFlow.doResume(ExpressionBuilderHelper.fromValue(results));
+		return InstructionFlow.doResume(ExpressionBuilderHelper.emptyList());
 	}
 
 	@Override
