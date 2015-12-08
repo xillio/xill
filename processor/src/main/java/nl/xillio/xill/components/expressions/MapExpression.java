4e58d0023d84faa06bcddcd499e7c83a8dc2e047
Pieter Dirk Soels <pieter.soels@xillio.nl>
2015-12-08 10:12:24 +0100

    Implemented same-type result map and filter

--- processor/src/main/java/nl/xillio/xill/components/expressions/MapExpression.java
+++ processor/src/main/java/nl/xillio/xill/components/expressions/MapExpression.java
@@ -1,17 +1,15 @@
 package nl.xillio.xill.components.expressions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 import nl.xillio.xill.api.Debugger;
+import nl.xillio.xill.api.components.ExpressionBuilder;
 import nl.xillio.xill.api.components.InstructionFlow;
 import nl.xillio.xill.api.components.MetaExpression;
 import nl.xillio.xill.api.components.Processable;
 import nl.xillio.xill.api.construct.ExpressionBuilderHelper;
+import nl.xillio.xill.api.errors.NotImplementedException;
 import nl.xillio.xill.api.errors.RobotRuntimeException;
 import nl.xillio.xill.components.instructions.FunctionDeclaration;
 
@@ -35,7 +33,8 @@ public class MapExpression implements Processable, FunctionParameterExpression {
 	@SuppressWarnings("unchecked")
 	@Override
 	public InstructionFlow<MetaExpression> process(final Debugger debugger) throws RobotRuntimeException {
-		List<MetaExpression> results = new ArrayList<>();
+		List<MetaExpression> listResults = new ArrayList<>();
+		LinkedHashMap<String, MetaExpression> objectResults = new LinkedHashMap<>();
 
 		// Call the function for all arguments
 		for (Processable argument : arguments) {
@@ -44,27 +43,25 @@ public class MapExpression implements Processable, FunctionParameterExpression {
 			switch (result.getType()) {
 				case ATOMIC:
 					// Process the one argument
-					results.add(functionDeclaration.run(debugger, Arrays.asList(result)).get());
-					break;
+					return InstructionFlow.doResume(functionDeclaration.run(debugger, Arrays.asList(result)).get());
 				case LIST:
 					// Pass every argument
 					for (MetaExpression expression : (List<MetaExpression>) result.getValue()) {
-						results.add(functionDeclaration.run(debugger, Arrays.asList(expression)).get());
+						listResults.add(functionDeclaration.run(debugger, Arrays.asList(expression)).get());
 					}
-					break;
+					return InstructionFlow.doResume(ExpressionBuilderHelper.fromValue(listResults));
 				case OBJECT:
 					// Pass every argument but with key
 					for (Entry<String, MetaExpression> expression : ((Map<String, MetaExpression>) result.getValue()).entrySet()) {
-						results.add(functionDeclaration.run(debugger, Arrays.asList(ExpressionBuilderHelper.fromValue(expression.getKey()), expression.getValue())).get());
+						objectResults.put(expression.getKey(), functionDeclaration.run(debugger, Arrays.asList(ExpressionBuilderHelper.fromValue(expression.getKey()), expression.getValue())).get());
 					}
-					break;
+					return InstructionFlow.doResume(ExpressionBuilderHelper.fromValue(objectResults));
 				default:
-					break;
+					throw new NotImplementedException("This MetaExpression has not been implemented yet");
 
 			}
 		}
-
-		return InstructionFlow.doResume(ExpressionBuilderHelper.fromValue(results));
+		return InstructionFlow.doResume(ExpressionBuilderHelper.emptyList());
 	}
 
 	@Override
