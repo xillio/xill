cf27fe722638c8e80992eefe0d64a50d8dda1bd9
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-12-14 16:04:47 +0100

    Revert "Revert "Implement services 1""
    This reverts commit dd8bdea03a2ec637bc1e289bded50c5e8bdf7aa5.

---
+++ xill-plugins/plugin-document/src/test/java/nl/xillio/xill/plugins/mongodb/services/ConnectionFactoryTest.java
@@ -0,0 +1,21 @@
+package nl.xillio.xill.plugins.mongodb.services;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+
+public class ConnectionFactoryTest {
+    @Test
+    public void testGetConnection () {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        ConnectionInfo info = new ConnectionInfo("localhost", 2345, "database");
+
+        Connection connection = connectionFactory.build(info);
+
+        assertNotNull(connection);
+        assertNotNull(connection.getClient());
+        assertFalse(connection.isClosed());
+    }
+}
