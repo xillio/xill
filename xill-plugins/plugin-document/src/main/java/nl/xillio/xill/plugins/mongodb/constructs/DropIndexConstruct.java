f4813542aae3e621381b34c9247b679d3771c997
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-12-15 16:03:28 +0100

    Add DropIndexConstruct

---
+++ xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/mongodb/constructs/DropIndexConstruct.java
@@ -0,0 +1,43 @@
+package nl.xillio.xill.plugins.mongodb.constructs;
+
+import com.mongodb.client.MongoCollection;
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.construct.Argument;
+import nl.xillio.xill.api.errors.RobotRuntimeException;
+import org.bson.Document;
+
+/**
+ * This construct represents the drop method on MongoDB.
+ *
+ * @author Thomas Biesaart
+ * @see <a href="https://docs.mongodb.org/v3.0/reference/method/db.collection.drop/#db.collection.drop">db.collection.drop</a>
+ */
+public class DropIndexConstruct extends AbstractCollectionApiConstruct {
+
+
+    @Override
+    protected Argument[] getApiArguments() {
+        return new Argument[]{
+                new Argument("index", ATOMIC, OBJECT)
+        };
+    }
+
+    @Override
+    MetaExpression process(MetaExpression[] arguments, MongoCollection<Document> collection) {
+
+        switch (arguments[0].getType()) {
+            case ATOMIC:
+                collection.dropIndex(arguments[0].getStringValue());
+                break;
+            case OBJECT:
+                Document index = getQuery(arguments[0]);
+                collection.dropIndex(index);
+                break;
+            default:
+                // This should never happen but it is there just in case
+                throw new RobotRuntimeException("Invalid Input! Please contact the development team.");
+        }
+
+        return NULL;
+    }
+}
