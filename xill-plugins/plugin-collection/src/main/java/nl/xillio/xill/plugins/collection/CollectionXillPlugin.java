3965744413db87a6ebf82d13385e65cdf7a8c1c9
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-12-17 13:40:12 +0100

    Rename javadoc reference

--- xill-plugins/plugin-collection/src/main/java/nl/xillio/xill/plugins/collection/CollectionXillPlugin.java
+++ xill-plugins/plugin-collection/src/main/java/nl/xillio/xill/plugins/collection/CollectionXillPlugin.java
@@ -3,7 +3,7 @@ package nl.xillio.xill.plugins.collection;
 import nl.xillio.plugins.XillPlugin;
 
 /**
- * This package includes all list constructs.
+ * This package includes all collection constructs.
  */
 public class CollectionXillPlugin extends XillPlugin {
 }
