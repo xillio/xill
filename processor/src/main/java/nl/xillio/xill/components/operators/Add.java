ad56e28ed867ea90444266768ef930e454443b0c
Pieter Dirk Soels <pieter.soels@xillio.nl>
2015-12-17 15:03:01 +0100

    - Implement atomic addition to list and object. - Added error on addition list or object to atomic.

--- processor/src/main/java/nl/xillio/xill/components/operators/Add.java
+++ processor/src/main/java/nl/xillio/xill/components/operators/Add.java
@@ -9,7 +9,6 @@ import nl.xillio.xill.api.errors.RobotRuntimeException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This class represents the + operation.
@@ -30,8 +29,19 @@ public final class Add extends BinaryNumberOperator {
         rightValue.registerReference();
 
         try {
+            //If you try to add a null value to anything it should give an error.
             if (leftValue.isNull() || rightValue.isNull()) {
                 throw new RobotRuntimeException("An addition has been tried upon a null value");
+            }
+
+            //If you try to add a list to an atomic value it should give an error since this is unwanted behaviour.
+            if (leftValue.getType() == ExpressionDataType.ATOMIC && rightValue.getType() == ExpressionDataType.LIST) {
+                throw new RobotRuntimeException("You can not add a list to an atomic value.");
+            }
+
+            //If you try to add an object to an atomic value it should give an error since this is unwanted behaviour.
+            if (leftValue.getType() == ExpressionDataType.ATOMIC && rightValue.getType() == ExpressionDataType.OBJECT) {
+                throw new RobotRuntimeException("You can not add an object to an atomic value.");
             }
 
             // If both entries are a list, then add them as such
@@ -40,25 +50,41 @@ public final class Add extends BinaryNumberOperator {
                         processLists((List<MetaExpression>) leftValue.getValue(),
                                 (List<MetaExpression>) rightValue.getValue(), debugger));
             }
+
             // If both entries are an object, then add them as such
             if (leftValue.getType() == rightValue.getType() && leftValue.getType() == ExpressionDataType.OBJECT) {
                 return InstructionFlow.doResume(
                         processObjects((LinkedHashMap<String, MetaExpression>) leftValue.getValue(),
                                 (LinkedHashMap<String, MetaExpression>) rightValue.getValue(), debugger));
             }
+            // If the left entry is a list and the right entry is an atomic value, Add the atomic value to the list.
 
-            if (leftValue.getType() == ExpressionDataType.LIST && rightValue.getType() == ExpressionDataType.OBJECT){
+            if (leftValue.getType() == ExpressionDataType.LIST && rightValue.getType() == ExpressionDataType.ATOMIC) {
+                return InstructionFlow.doResume(
+                        addToList((List<MetaExpression>) leftValue.getValue(), rightValue, debugger));
+            }
+            // If the left entry is an object and the right entry is an atomic value, Add the atomic value to the object.
+            if (leftValue.getType() == ExpressionDataType.OBJECT && rightValue.getType() == ExpressionDataType.ATOMIC) {
+                return InstructionFlow.doResume(
+                        addToObject((LinkedHashMap<String, MetaExpression>) leftValue.getValue(), rightValue, debugger));
+            }
+            // If the left entry is a list and the right entry is an object,
+            // then convert the list to an object and put it in front of the object.
+            if (leftValue.getType() == ExpressionDataType.LIST && rightValue.getType() == ExpressionDataType.OBJECT) {
                 return InstructionFlow.doResume(
                         processListObject((List<MetaExpression>) leftValue.getValue(),
                                 (LinkedHashMap<String, MetaExpression>) rightValue.getValue(), debugger));
             }
 
-            if (leftValue.getType() == ExpressionDataType.OBJECT && rightValue.getType() == ExpressionDataType.LIST){
+            // If the left entry is an object and the right entry is a list,
+            // then convert the list to an object and put it at the end of the object.
+            if (leftValue.getType() == ExpressionDataType.OBJECT && rightValue.getType() == ExpressionDataType.LIST) {
                 return InstructionFlow.doResume(
                         processObjectList((LinkedHashMap<String, MetaExpression>) leftValue.getValue(),
                                 (List<MetaExpression>) rightValue.getValue(), debugger));
             }
 
+            // The left and right entries are atomics so add them as numbers.
             return super.process(leftValue, rightValue);
         } finally {
             rightValue.releaseReference();
@@ -75,7 +101,7 @@ public final class Add extends BinaryNumberOperator {
     }
 
     private static MetaExpression processObjects(final LinkedHashMap<String, MetaExpression> leftValue, final LinkedHashMap<String, MetaExpression> rightValue, final Debugger debugger) throws RobotRuntimeException {
-        LinkedHashMap<String, MetaExpression> result = leftValue;
+        LinkedHashMap<String, MetaExpression> result = new LinkedHashMap<>(leftValue);
         result.putAll(rightValue);
 
         return ExpressionBuilderHelper.fromValue(result);
@@ -97,6 +123,18 @@ public final class Add extends BinaryNumberOperator {
             result.put(Integer.toString(i), rightValue.get(i - leftValue.size()));
         }
 
+        return ExpressionBuilderHelper.fromValue(result);
+    }
+
+    private static MetaExpression addToList(final List<MetaExpression> leftValue, final MetaExpression rightValue, final Debugger debugger) throws RobotRuntimeException {
+        List<MetaExpression> result = new ArrayList<>(leftValue);
+        result.add(rightValue);
+        return ExpressionBuilderHelper.fromValue(result);
+    }
+
+    private static MetaExpression addToObject(final LinkedHashMap<String, MetaExpression> leftValue,final MetaExpression rightValue,final Debugger debugger) throws RobotRuntimeException {
+        LinkedHashMap<String, MetaExpression> result = new LinkedHashMap<>(leftValue);
+        result.put(Integer.toString(leftValue.size()), rightValue);
         return ExpressionBuilderHelper.fromValue(result);
     }
 }
