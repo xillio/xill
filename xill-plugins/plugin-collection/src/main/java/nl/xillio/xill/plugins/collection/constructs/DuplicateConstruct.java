d5a12b2ca7b6f08e4cc922901e4359622a628bb6
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-12-17 13:25:15 +0100

    Rename List plugin to Collection

---
+++ xill-plugins/plugin-collection/src/main/java/nl/xillio/xill/plugins/collection/constructs/DuplicateConstruct.java
@@ -0,0 +1,44 @@
+package nl.xillio.xill.plugins.collection.constructs;
+
+import com.google.inject.Inject;
+
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.construct.Argument;
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+import nl.xillio.xill.plugins.collection.services.duplicate.Duplicate;
+
+/**
+ * Returns a deep copy of the given list or object.
+ *
+ * @author Sander Visser
+ *
+ */
+public class DuplicateConstruct extends Construct {
+
+	@Inject
+	private Duplicate duplicate;
+	
+	@Override
+	public ConstructProcessor prepareProcess(final ConstructContext context) {
+		return new ConstructProcessor(
+			input -> process(input,duplicate),
+			new Argument("list", LIST, OBJECT));
+	}
+
+	/**
+	 * Returns a deep copy of the given list or object.
+	 *
+	 * @param input
+	 *        the list or object.
+	 * @return the deep copy of the list or object.
+	 */
+	static MetaExpression process(final MetaExpression input,final Duplicate duplicate) {
+		Object obj = extractValue(input);
+		obj = duplicate.duplicate(obj);
+		MetaExpression output = MetaExpression.parseObject(obj);
+		return output;
+		
+	}
+}
