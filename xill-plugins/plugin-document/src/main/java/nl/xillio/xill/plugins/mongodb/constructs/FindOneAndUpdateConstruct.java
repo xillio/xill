4c3e3973896ea4a489815f4ff12a5f829078a726
Titus Nachbauer <titus.nachbauer@xillio.nl>
2015-12-21 16:12:29 +0100

    Add FindOneAndUpdate construct

---
+++ xill-plugins/plugin-document/src/main/java/nl/xillio/xill/plugins/mongodb/constructs/FindOneAndUpdateConstruct.java
@@ -0,0 +1,44 @@
+package nl.xillio.xill.plugins.mongodb.constructs;
+
+import com.google.inject.Inject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.construct.Argument;
+import nl.xillio.xill.plugins.mongodb.services.FindOneAndUpdateOptionsFactory;
+import org.bson.Document;
+
+/**
+ * This construct represents the <code>FindOneAndUpdate()</code> method on MongoDB. That method is specific to the Java
+ * API of MongoDB and therefore not documented in the online javascript API documentation. It replaces the
+ * <code>FindAndModify()</code> method.
+ *
+ * @author Titus Nachbauer
+ */
+public class FindOneAndUpdateConstruct extends AbstractCollectionApiConstruct {
+    private final FindOneAndUpdateOptionsFactory findOneAndUpdateOptionsFactory;
+
+    @Inject
+    public FindOneAndUpdateConstruct(FindOneAndUpdateOptionsFactory findOneAndUpdateOptionsFactory) {
+        this.findOneAndUpdateOptionsFactory = findOneAndUpdateOptionsFactory;
+    }
+
+    @Override
+    protected Argument[] getApiArguments() {
+        return new Argument[]{
+                new Argument("query", emptyObject(), OBJECT),
+                new Argument("update", emptyObject(), OBJECT),
+                new Argument("options", emptyObject(), OBJECT)
+        };
+    }
+
+    @Override
+    MetaExpression process(MetaExpression[] arguments, MongoCollection<Document> collection) {
+        Document filter = toDocument(arguments[0]);
+        Document update = toDocument(arguments[1]);
+        FindOneAndUpdateOptions options = findOneAndUpdateOptionsFactory.build(arguments[2]);
+
+        Document result = collection.findOneAndUpdate(filter, update, options);
+        return toExpression(result);
+    }
+}
---
+++ xill-plugins/plugin-document/src/test/java/nl/xillio/xill/plugins/mongodb/services/FindOneAndUpdateOptionsFactoryTest.java
@@ -0,0 +1,56 @@
+package nl.xillio.xill.plugins.mongodb.services;
+
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import nl.xillio.xill.TestUtils;
+import nl.xillio.xill.api.components.MetaExpression;
+import org.testng.annotations.Test;
+
+import java.util.LinkedHashMap;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.*;
+
+public class FindOneAndUpdateOptionsFactoryTest extends TestUtils {
+    @Test
+    public void testBuildOptions () {
+        MongoConverter mongoConverter = new MongoConverter();
+        FindOneAndUpdateOptionsFactory FindOneAndUpdateOptionsFactory = new FindOneAndUpdateOptionsFactory(mongoConverter);
+        LinkedHashMap<String, MetaExpression> object = new LinkedHashMap<>();
+
+        FindOneAndUpdateOptions options = FindOneAndUpdateOptionsFactory.build(fromValue(object));
+        LinkedHashMap<String, MetaExpression> projection = new LinkedHashMap<>();
+
+        assertNull(options.getProjection());
+        projection.put("_id", FALSE);
+        projection.put("target", TRUE);
+        object.put("projection", fromValue(projection));
+        options = FindOneAndUpdateOptionsFactory.build(fromValue(object));
+        assertNotNull(options.getProjection());
+        assertEquals(MetaExpression.parseObject(options.getProjection()), object.get("projection"));
+
+        assertNull(options.getSort());
+        LinkedHashMap<String, MetaExpression> sort = new LinkedHashMap<>();
+        sort.put("target.timestamp", fromValue(-1));
+        object.put("sortBy", fromValue(sort));
+        options = FindOneAndUpdateOptionsFactory.build(fromValue(object));
+        assertNotNull(options.getSort());
+        assertEquals(MetaExpression.parseObject(options.getSort()), object.get("sortBy"));
+
+        assertFalse(options.isUpsert());
+        object.put("upsert", TRUE);
+        options = FindOneAndUpdateOptionsFactory.build(fromValue(object));
+        assertTrue(options.isUpsert());
+
+        assertEquals(options.getReturnDocument(), ReturnDocument.BEFORE);
+        object.put("returnNew", TRUE);
+        options = FindOneAndUpdateOptionsFactory.build(fromValue(object));
+        assertEquals(options.getReturnDocument(), ReturnDocument.AFTER);
+
+        assertEquals(options.getMaxTime(TimeUnit.MILLISECONDS), 0);
+        object.put("maxTime", fromValue(2000));
+        options = FindOneAndUpdateOptionsFactory.build(fromValue(object));
+        assertNotNull(options.getMaxTime(TimeUnit.MILLISECONDS));
+        assertEquals(MetaExpression.parseObject(options.getMaxTime(TimeUnit.MILLISECONDS)), object.get("maxTime"));
+    }
+}
