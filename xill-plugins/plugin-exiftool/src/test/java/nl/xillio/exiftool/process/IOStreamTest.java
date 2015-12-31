a10951e0c4c562fa8979950a6424a05326abb146
Pieter Dirk Soels <pieter.soels@xillio.nl>
2015-12-31 10:59:53 +0100

    Unit test IOStream

---
+++ xill-plugins/plugin-exiftool/src/test/java/nl/xillio/exiftool/process/IOStreamTest.java
@@ -0,0 +1,67 @@
+package nl.xillio.exiftool.process;
+
+import static org.mockito.Mockito.*;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static org.testng.Assert.*;
+
+/**
+ * Unit test for closing IO streams.
+ *
+ * @author Pieter Dirk Soels
+ */
+public class IOStreamTest {
+
+    @Test
+    public void testCloseNormalConditions() throws IOException {
+        // Mock dependencies
+        BufferedReader input = mock(BufferedReader.class);
+        OutputStreamWriter output = mock(OutputStreamWriter.class);
+        IOStream stream = new IOStream(input, output);
+
+        // Run
+        stream.close();
+
+        // Verify
+        verify(input, times(1)).close();
+        verify(output, times(1)).close();
+    }
+
+    @Test
+    public void testCloseInputException() throws IOException {
+        // Mock dependencies
+        BufferedReader input = mock(BufferedReader.class);
+        OutputStreamWriter output = mock(OutputStreamWriter.class);
+        IOStream stream = new IOStream(input, output);
+
+        doThrow(IOException.class).when(input).close();
+
+        // Run
+        stream.close();
+
+        // Verify
+        verify(input, times(1)).close();
+        verify(output, times(1)).close();
+    }
+
+    @Test
+    public void testCloseOutputException() throws IOException {
+        // Mock dependencies
+        BufferedReader input = mock(BufferedReader.class);
+        OutputStreamWriter output = mock(OutputStreamWriter.class);
+        IOStream stream = new IOStream(input, output);
+
+        doThrow(IOException.class).when(output).close();
+
+        // Run
+        stream.close();
+
+        // Verify
+        verify(input, times(1)).close();
+        verify(output, times(1)).close();
+    }
+}
