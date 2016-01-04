a0f1b3077c459fbc3d831e599684dc0159144368
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-01-04 10:21:44 +0100

    Move java wrapper to own repository

--- xill-plugins/plugin-exiftool/src/main/java/nl/xillio/exiftool/query/FileQueryOptions.java
+++
@@ -1,9 +0,0 @@
-package nl.xillio.exiftool.query;
-
-/**
- * This interface represents options for a query on a single file.
- *
- * @author Thomas Biesaart
- */
-public interface FileQueryOptions extends QueryOptions {
-}
