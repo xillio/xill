cf46bb4c07b1614c71c80feceff3ac7abb5d5832
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-01-07 10:41:43 +0100

    Revert "Clean up"
    This reverts commit 4d2709a3cdfaffa15e39bc645f17fe6a8d615e2d.

--- ide/src/main/java/nl/xillio/migrationtool/gui/editor/XillJSObject.java
+++ ide/src/main/java/nl/xillio/migrationtool/gui/editor/XillJSObject.java
@@ -6,6 +6,7 @@ import nl.xillio.xill.api.XillProcessor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Collection;
 import java.util.Objects;
 
 public class XillJSObject {
@@ -22,19 +23,30 @@ public class XillJSObject {
     }
 
     public void getCompletions(JSObject query) {
-        String prefix = query.getMember("prefix").toString();
-        String currentLine = query.getMember("currentLine").toString();
-        int column = ((Number) query.getMember("column")).intValue();
-        int row = ((Number) query.getMember("row")).intValue();
 
-        processor.getCompletions(currentLine, prefix, column, row).forEach(
-                (key, value) -> value.forEach(
-                        item -> {
-                            int parPos = item.lastIndexOf("(");
-                            String caption = parPos > 0 ? item.substring(0, parPos) : item;
-                            query.call("callback", caption, item, 10, key);
-                        }
-                )
-        );
+        try {
+            String prefix = query.getMember("prefix").toString();
+            String currentLine = query.getMember("currentLine").toString();
+            int column = ((Number) query.getMember("column")).intValue();
+            int row = ((Number) query.getMember("row")).intValue();
+
+            processor.getCompletions(currentLine, prefix, column, row).forEach(
+                    (key, value) -> value.forEach(
+                            item -> {
+                                int parPos = item.lastIndexOf("(");
+                                String caption = parPos > 0 ? item.substring(0, parPos) : item;
+                                query.call("callback", caption, item, 10, key);
+                            }
+                    )
+            );
+        } catch (Exception e) {
+            LOGGER.error("ugh", e);
+            throw e;
+        }
+    }
+
+    public String[] getPluginNames() {
+        Collection<String> names = processor.listPackages();
+        return names.toArray(new String[names.size()]);
     }
 }
