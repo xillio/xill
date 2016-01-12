17ab5d8d23e8683dc36619b373b31f974e0e219b
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-01-12 15:14:31 +0100

    Fix exception reference

--- xill-plugins/plugin-excel/src/test/java/test/integration/Excel.java
+++ xill-plugins/plugin-excel/src/test/java/test/integration/Excel.java
@@ -5,6 +5,7 @@ import nl.xillio.xill.RobotIntegrationTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class Excel extends RobotIntegrationTest {
@@ -16,7 +17,7 @@ public class Excel extends RobotIntegrationTest {
 
     @Test(dataProvider = "myRobots")
     @Override
-    public void runRobot(URL robot, String name) throws Exception {
+    public void runRobot(URL robot, String name) throws IOException {
         super.runRobot(robot, name);
     }
 }
