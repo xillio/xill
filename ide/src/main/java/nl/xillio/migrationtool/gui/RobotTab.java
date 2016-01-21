0aecb8efa40f7740973662357fd8f05c40b17725
sanvisser <sanvisser@gmail.com>
2016-01-21 10:11:24 +0100

    Rename timeline
    Renamed the timeline variable to autoSaveTimeline

--- ide/src/main/java/nl/xillio/migrationtool/gui/RobotTab.java
+++ ide/src/main/java/nl/xillio/migrationtool/gui/RobotTab.java
@@ -55,7 +55,7 @@ public class RobotTab extends Tab implements Initializable, ChangeListener<Docum
     private static final String PATH_STATUSICON_PAUSED = "M256,92.481c44.433,0,86.18,17.068,117.553,48.064C404.794,171.411,422,212.413,422,255.999 s-17.206,84.588-48.448,115.455c-31.372,30.994-73.12,48.064-117.552,48.064s-86.179-17.07-117.552-48.064 C107.206,340.587,90,299.585,90,255.999s17.206-84.588,48.448-115.453C169.821,109.55,211.568,92.481,256,92.481 M256,52.481 c-113.771,0-206,91.117-206,203.518c0,112.398,92.229,203.52,206,203.52c113.772,0,206-91.121,206-203.52 C462,143.599,369.772,52.481,256,52.481L256,52.481z M240.258,346h-52.428V166h52.428V346z M326.17,346h-52.428V166h52.428V346z";
     private final Group STATUSICON_RUNNING = createIcon(PATH_STATUSICON_RUNNING);
     private final Group STATUSICON_PAUSED = createIcon(PATH_STATUSICON_PAUSED);
-    private Timeline timeline;
+    private Timeline autoSaveTimeline;
 
     @FXML
     private HBox hbxBot;
@@ -110,7 +110,7 @@ public class RobotTab extends Tab implements Initializable, ChangeListener<Docum
         // Add the context menu.
         addContextMenu(globalController);
 
-        timeline = new Timeline(new KeyFrame(
+        autoSaveTimeline = new Timeline(new KeyFrame(
                 Duration.millis(1000),
                 ae -> save(false)));
 
@@ -122,7 +122,7 @@ public class RobotTab extends Tab implements Initializable, ChangeListener<Docum
      */
     public void resetAutoSave(){
         if(Boolean.valueOf(settings.simple().get("SettingsGeneral", "EnableAutoSave"))){
-            this.timeline.playFromStart();
+            this.autoSaveTimeline.playFromStart();
         }
     }
 
@@ -346,7 +346,7 @@ public class RobotTab extends Tab implements Initializable, ChangeListener<Docum
 
         return true;
     }
-    
+
     private void validate() {
         List<Issue> issues = getProcessor().validate()
                 .stream()
