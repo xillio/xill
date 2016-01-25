e2bc0a70afa80c6e2b61ba23bdb26f59e831521e
Thomas Biesaart <thomas.biesaart@outlook.com>
2016-01-25 09:01:39 +0100

    Start implementation of generic loader

---
+++ api/src/main/java/nl/xillio/xill/api/XillEnvironment.java
@@ -0,0 +1,47 @@
+package nl.xillio.xill.api;
+
+import com.google.inject.Injector;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * This interface represents an object that can assemble {@link XillProcessor}.
+ *
+ * @author Thomas Biesaart
+ */
+public interface XillEnvironment {
+    /**
+     * Enable or disable loading plugins from the user's home folder.
+     *
+     * @param value true if plugins from home folder should be loaded
+     * @return self
+     */
+    XillEnvironment setLoadHomeFolder(boolean value);
+
+    /**
+     * Add a folder to the plugin loading queue.
+     *
+     * @param path the path to the folder
+     * @return self
+     * @throws IOException if the provided path is not a folder or doesn't exist
+     */
+    XillEnvironment addFolder(Path path) throws IOException;
+
+    /**
+     * Set the root injector for the plugin environment.
+     *
+     * @param injector the injector
+     * @return self
+     */
+    XillEnvironment setRootInjector(Injector injector);
+
+    /**
+     * Build a processor for a specific execution.
+     *
+     * @param projectRoot the root folder of the project
+     * @param robotPath   the path to the main robot
+     * @return the processor
+     */
+    XillProcessor buildProcessor(Path projectRoot, Path robotPath) throws IOException;
+}
---
+++ processor/src/main/java/nl/xillio/xill/XillEnvironmentImpl.java
@@ -0,0 +1,136 @@
+package nl.xillio.xill;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import me.biesaart.utils.Log;
+import nl.xillio.plugins.XillPlugin;
+import nl.xillio.util.XillioHomeFolder;
+import nl.xillio.xill.api.XillEnvironment;
+import nl.xillio.xill.api.XillProcessor;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+/**
+ * This class is responsible for loading the processor and it's plugins.
+ *
+ * @author Thomas Biesaart
+ */
+public class XillEnvironmentImpl implements XillEnvironment {
+    private static final Path HOME_PLUGIN_DIR = Paths.get(XillioHomeFolder.forXill3().toURI()).resolve("plugins");
+    private static final Logger LOGGER = Log.get();
+    private boolean loadHome = true;
+    private final List<Path> folders = new ArrayList<>();
+    private Injector rootInjector;
+    private Map<String, XillPlugin> loadedPlugins = new HashMap<>();
+    private boolean needLoad = true;
+
+    @Override
+    public XillEnvironment setLoadHomeFolder(boolean value) {
+        loadHome = value;
+        return this;
+    }
+
+    @Override
+    public XillEnvironment addFolder(Path path) throws IOException {
+        folders.add(path);
+        return this;
+    }
+
+    @Override
+    public XillEnvironment setRootInjector(Injector injector) {
+        rootInjector = injector;
+        return this;
+    }
+
+    public static void main(String... args) throws IOException {
+        new XillEnvironmentImpl()
+                .addFolder(Paths.get("C:\\Library\\Code\\Xillio\\xill\\plugins"))
+                .buildProcessor(Paths.get("."), Paths.get("."));
+    }
+
+    @Override
+    public XillProcessor buildProcessor(Path projectRoot, Path robotPath) throws IOException {
+        if (needLoad) {
+            if (rootInjector == null) {
+                rootInjector = Guice.createInjector();
+            }
+
+            List<Path> folders = new ArrayList<>(this.folders);
+            if (loadHome && Files.exists(HOME_PLUGIN_DIR)) {
+                folders.add(HOME_PLUGIN_DIR);
+            }
+            loadClasspathPlugins();
+            loadPlugins(folders);
+            needLoad = false;
+        }
+
+        return null;
+    }
+
+    private void loadClasspathPlugins() {
+        loadPlugins(ServiceLoader.load(XillPlugin.class));
+    }
+
+    private void loadPlugins(Iterable<Path> folders) throws IOException {
+        for (Path folder : folders) {
+            assertFolderExists(folder);
+
+            Processor processor = new Processor();
+            Files.walkFileTree(folder, processor);
+            for (Path jarFile : processor.getJarFiles()) {
+                URL url = jarFile.toUri().toURL();
+                URLClassLoader classLoader = new URLClassLoader(new URL[]{url});
+                ServiceLoader<XillPlugin> serviceLoader = ServiceLoader.load(XillPlugin.class, classLoader);
+                loadPlugins(serviceLoader);
+            }
+        }
+    }
+
+    private void loadPlugins(ServiceLoader<XillPlugin> serviceLoader) {
+        for (XillPlugin plugin : serviceLoader) {
+            String name = plugin.getName();
+            if (!loadedPlugins.containsKey(name)) {
+                LOGGER.info("Found plugin {}", name);
+                loadedPlugins.put(name, plugin);
+            }
+        }
+    }
+
+    private void assertFolderExists(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            throw new NoSuchFileException("No folder found at " + path);
+        }
+
+        if (!Files.isDirectory(path)) {
+            throw new NotDirectoryException(path + " is not a directory");
+        }
+
+    }
+
+    /**
+     * This class will save a list of all .jar files.
+     */
+    private static class Processor extends SimpleFileVisitor<Path> {
+        private static final PathMatcher JAR_MATCHER = FileSystems.getDefault().getPathMatcher("glob:**.jar");
+        private final List<Path> jarFiles = new ArrayList<>();
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (JAR_MATCHER.matches(file)) {
+                jarFiles.add(file);
+            }
+            return super.visitFile(file, attrs);
+        }
+
+        public List<Path> getJarFiles() {
+            return jarFiles;
+        }
+    }
+
+}
