f919aa7467421e7b33fec34e04e921b49121a65d
Anwar Rahimbaks <anwar.rahimbaks@xillio.nl>
2016-02-18 12:10:40 +0100

    Updated the comment for getting a logger instance.
    CTC-1196.

--- api/src/main/java/nl/xillio/xill/api/LogUtil.java
+++ api/src/main/java/nl/xillio/xill/api/LogUtil.java
@@ -13,6 +13,12 @@ public class LogUtil {
 
     public static final String ROBOT_LOGGER_PREFIX = "robot.";
 
+    /**
+     * Get a new logger instance associated with a specific robot ID
+     *
+     * @param robotID ID of a robot
+     * @return        An instance of the logger.
+     */
     public Logger getLogger(final RobotID robotID) {
         return LoggerFactory.getLogger(ROBOT_LOGGER_PREFIX + robotID.toString());
     }
