98463bc004c20237de784cfea719ec926181addb
Luca Scalzotto <lucascalzotto@gmail.com>
2016-03-16 13:33:42 +0100

    Create byteLength construct, unit tests and documentation

---
+++ processor/src/test/java/nl/xillio/xill/plugins/string/constructs/ByteLengthConstructTest.java
@@ -0,0 +1,47 @@
+package nl.xillio.xill.plugins.string.constructs;
+
+import nl.xillio.xill.TestUtils;
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.errors.RobotRuntimeException;
+import nl.xillio.xill.plugins.string.services.string.StringUtilityService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.internal.collections.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Test the {@link ByteLengthConstruct}.
+ */
+public class ByteLengthConstructTest extends TestUtils {
+
+    /**
+     * Test the process method under normal circumstances.
+     */
+    @Test
+    public void processNormalUsage() {
+        MetaExpression string = fromValue("foo");
+
+        // Testing pairs of encoding & byte length.
+        List<Pair<MetaExpression, Integer>> testPairs = new ArrayList<>();
+        testPairs.add(Pair.create(NULL, 3)); // Should be the same as UTF-8.
+        testPairs.add(Pair.create(fromValue("ASCII"), 3));
+        testPairs.add(Pair.create(fromValue("UTF-8"), 3));
+        testPairs.add(Pair.create(fromValue("UTF-16"), 8));
+        testPairs.add(Pair.create(fromValue("UTF-32"), 12));
+
+        // Test each pair.
+        testPairs.forEach(p -> Assert.assertEquals(ByteLengthConstruct.process(string, p.first()).getNumberValue().intValue(), (int)p.second()));
+    }
+
+    /**
+     * Test the process when the given string is null.
+     */
+    @Test(expectedExceptions = RobotRuntimeException.class)
+    public void processNullValue() {
+        ByteLengthConstruct.process(NULL, NULL);
+    }
+}
