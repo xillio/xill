3d050f273a9a3f2dab5c837268a50b3717c9092e
Edward van Egdom <edward.vanegdom@xillio.nl>
2016-04-11 12:50:25 +0200

    Use buffered input stream to speed up IO

--- processor/src/main/java/nl/xillio/xill/plugins/system/exec/InputStreamReaderCallable.java
+++ processor/src/main/java/nl/xillio/xill/plugins/system/exec/InputStreamReaderCallable.java
@@ -3,16 +3,16 @@ package nl.xillio.xill.plugins.system.exec;
 import me.biesaart.utils.Log;
 import org.slf4j.Logger;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
 
 /**
  * This class will read all characters and returns the result in a string
  */
 public class InputStreamReaderCallable implements Callable<String> {
-    private final InputStreamReader input;
+    private final BufferedInputStream input;
     private static final Logger LOGGER = Log.get();
 
     /**
@@ -20,9 +20,8 @@ public class InputStreamReaderCallable implements Callable<String> {
      *
      * @param input the input
      */
-    public
-    InputStreamReaderCallable(final InputStream input) {
-        this.input = new InputStreamReader(input);
+    public InputStreamReaderCallable(final InputStream input) {
+        this.input = new BufferedInputStream(input);
     }
 
     @Override
