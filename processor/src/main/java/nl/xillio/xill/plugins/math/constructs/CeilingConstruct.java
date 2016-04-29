6eb17f6ebbcee2141bb30b3866814e77d15fa53f
Luca Scalzotto <lucascalzotto@gmail.com>
2016-04-29 13:57:32 +0200

    Add floor and ceiling construct including unit tests and documentation

---
+++ processor/src/main/java/nl/xillio/xill/plugins/math/constructs/CeilingConstruct.java
@@ -0,0 +1,26 @@
+package nl.xillio.xill.plugins.math.constructs;
+
+import com.google.inject.Inject;
+import nl.xillio.xill.api.components.MetaExpression;
+import nl.xillio.xill.api.construct.Argument;
+import nl.xillio.xill.api.construct.Construct;
+import nl.xillio.xill.api.construct.ConstructContext;
+import nl.xillio.xill.api.construct.ConstructProcessor;
+import nl.xillio.xill.plugins.math.services.math.MathOperations;
+
+public class CeilingConstruct extends Construct {
+    @Inject
+    private MathOperations mathService;
+
+    @Override
+    public ConstructProcessor prepareProcess(final ConstructContext context) {
+        return new ConstructProcessor(
+                value -> process(value, mathService),
+                new Argument("value", ATOMIC));
+    }
+
+    static MetaExpression process(final MetaExpression value, final MathOperations math) {
+        return fromValue(math.ceiling(value.getNumberValue()));
+    }
+
+}
