516d287ce81a99f70856f5280132b8183482ac5e
Zbynek Hochmann <zbynek.hochmann@xillio.com>
2016-05-04 12:57:01 +0200

    Introduce new exceptions and add getShortMessage to RobotRuntimeException

---
+++ api/src/main/java/nl/xillio/xill/api/errors/InvalidUserInputException.java
@@ -0,0 +1,51 @@
+package nl.xillio.xill.api.errors;
+
+/**
+ * The specific exception whenever the input from the user is not valid
+ */
+public class InvalidUserInputException extends RobotRuntimeException {
+
+    protected String reason;
+
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param reason the short reason for this exception
+     * @param actualInput the representation of the input value that was not valid
+     * @param expectedInput the explanation of what is correct input (noun with article)
+     * @param example the example of correct usage in free form
+     * @param cause the cause
+     */
+    public InvalidUserInputException(final String reason, final String actualInput, final String expectedInput, final String example, final Throwable cause) {
+        super(reason + "\n\n== Actual Input ==\n" + actualInput + "\n\n== Expected Input ==\n" + expectedInput + (example == null ? "" : "\n\n== Example ==\n"+example), cause);
+        this.reason = reason;
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param reason the short reason for this exception
+     * @param actualInput the representation of the input value that was not valid
+     * @param expectedInput the explanation of what is correct input (noun with article)
+     * @param example the example of correct usage in free form
+     */
+    public InvalidUserInputException(final String reason, final String actualInput, final String expectedInput, final String example) {
+        this(reason, actualInput, expectedInput, example, null);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message without example.
+     *
+     * @param reason the short reason for this exception
+     * @param actualInput the representation of the input value that was not valid
+     * @param expectedInput the explanation of what is correct input (noun with article)
+     */
+    public InvalidUserInputException(final String reason, final String actualInput, final String expectedInput) {
+        this(reason, actualInput, expectedInput, null, null);
+    }
+
+    @Override
+    public String getShortMessage() {
+        return reason;
+    }
+}
---
+++ api/src/main/java/nl/xillio/xill/api/errors/OperationFailedException.java
@@ -0,0 +1,50 @@
+package nl.xillio.xill.api.errors;
+
+/**
+ * The specific exception whenever a certain operation could not be performed.
+ */
+public class OperationFailedException extends RobotRuntimeException {
+
+    protected String reason;
+    protected String operation;
+
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     *
+     * @param operation the operation that failed (imperative form without explicit casing)
+     * @param reason what went wrong (full human readable sentence)
+     * @param advice next course of action to solve the problem (human readable)
+     * @param cause the cause
+     */
+    public OperationFailedException(final String operation, final String reason, final String advice, final Throwable cause) {
+        super("Could not " + operation + ".\n\n== Reason ==\n" + reason + (advice == null ? "" : " " + advice), cause);
+        this.reason = reason;
+        this.operation = operation;
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param operation the operation that failed (imperative form without explicit casing)
+     * @param reason what went wrong (full human readable sentence)
+     * @param advice next course of action to solve the problem (human readable)
+     */
+    public OperationFailedException(final String operation, final String reason, final String advice) {
+        this(operation, reason, advice, null);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message without advice.
+     *
+     * @param operation the operation that failed (imperative form without explicit casing)
+     * @param reason what went wrong (full human readable sentence)
+     */
+    public OperationFailedException(final String operation, final String reason) {
+        this(operation, reason, null, null);
+    }
+
+    @Override
+    public String getShortMessage() {
+        return "Could not " + operation + ". " + reason;
+    }
+}
