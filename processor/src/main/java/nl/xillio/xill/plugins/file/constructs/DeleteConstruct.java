8ecc73d3c785d515a5964760980e0ec190ce4fb4
Zbynek Hochmann <zbynek.hochmann@xillio.com>
2016-05-20 13:04:17 +0200

    Resolve Sonar issues

--- processor/src/main/java/nl/xillio/xill/plugins/file/constructs/DeleteConstruct.java
+++ processor/src/main/java/nl/xillio/xill/plugins/file/constructs/DeleteConstruct.java
@@ -8,7 +8,6 @@ import nl.xillio.xill.api.construct.Construct;
 import nl.xillio.xill.api.construct.ConstructContext;
 import nl.xillio.xill.api.construct.ConstructProcessor;
 import nl.xillio.xill.api.errors.OperationFailedException;
-import nl.xillio.xill.api.errors.RobotRuntimeException;
 import nl.xillio.xill.plugins.file.services.files.FileUtilities;
 
 import java.io.IOException;
