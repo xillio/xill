74e9ab1fc9df523d7685f2f6fce795cd911153e6
Zbynek Hochmann <zbynek.hochmann@xillio.com>
2016-05-23 13:19:38 +0200

    Merge fix

--- processor/src/main/java/nl/xillio/xill/plugins/system/constructs/ExecConstruct.java
+++ processor/src/main/java/nl/xillio/xill/plugins/system/constructs/ExecConstruct.java
@@ -9,6 +9,7 @@ import nl.xillio.xill.api.construct.ConstructContext;
 import nl.xillio.xill.api.construct.ConstructProcessor;
 import nl.xillio.xill.api.errors.InvalidUserInputException;
 import nl.xillio.xill.api.errors.OperationFailedException;
+import nl.xillio.xill.api.errors.RobotRuntimeException;
 import nl.xillio.xill.plugins.system.exec.InputStreamReaderCallable;
 import nl.xillio.xill.plugins.system.exec.ProcessDescription;
 import nl.xillio.xill.plugins.system.exec.ProcessFactory;
