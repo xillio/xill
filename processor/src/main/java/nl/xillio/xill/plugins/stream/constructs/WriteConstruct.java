9064d2bce00e56b724c11773a2d9626457d71b50
Geert Konijnendijk <geert@konijnendijk.info>
2016-06-23 13:31:38 +0200

    Add support for specifying input and output character sets in Stream.write

--- processor/src/main/java/nl/xillio/xill/plugins/stream/constructs/WriteConstruct.java
+++ processor/src/main/java/nl/xillio/xill/plugins/stream/constructs/WriteConstruct.java
@@ -9,9 +9,8 @@ import nl.xillio.xill.api.errors.InvalidUserInputException;
 import nl.xillio.xill.api.errors.OperationFailedException;
 import org.apache.commons.io.IOUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.Charset;
 
 import static nl.xillio.xill.plugins.stream.utils.StreamUtils.getInputStream;
 import static nl.xillio.xill.plugins.stream.utils.StreamUtils.getOutputStream;
@@ -30,12 +29,14 @@ class WriteConstruct extends Construct {
                 this::process,
                 new Argument("source", ATOMIC),
                 new Argument("target", ATOMIC),
-                new Argument("limit", fromValue(-1), ATOMIC)
+                new Argument("limit", fromValue(-1), ATOMIC),
+                new Argument("outputCharset", NULL, ATOMIC),
+                new Argument("inputCharset", NULL, ATOMIC)
         );
     }
 
     @SuppressWarnings("squid:UnusedPrivateMethod") // Sonar does not detect method references
-    private MetaExpression process(MetaExpression source, MetaExpression target, MetaExpression limit) {
+    private MetaExpression process(MetaExpression source, MetaExpression target, MetaExpression limit, MetaExpression outputCharset, MetaExpression inputCharset) {
 
         if (Double.isNaN(limit.getNumberValue().doubleValue())) {
             throw new InvalidUserInputException("The passed 'limit' parameter is not a valid number.", limit.getStringValue(), "A valid number.",
@@ -47,7 +48,12 @@ class WriteConstruct extends Construct {
         InputStream inputStream = openInputStream(source);
         OutputStream outputStream = getOutputStream(target, "target");
 
-        long dataCount = write(inputStream, outputStream, limit.getNumberValue().longValue());
+        // Ignore input encoding if the input is a string (not a stream)
+        String inputCharsetName = source.getBinaryValue().hasInputStream() ? inputCharset.getStringValue() : null;
+        String outputCharsetName = outputCharset.getStringValue();
+
+        long dataCount = write(inputStream, outputStream, outputCharsetName, inputCharsetName,
+                limit.getNumberValue().longValue());
         return fromValue(dataCount);
     }
 
@@ -58,9 +64,20 @@ class WriteConstruct extends Construct {
         return IOUtils.toInputStream(source.getStringValue());
     }
 
-    private long write(InputStream inputStream, OutputStream outputStream, long limit) {
+    private long write(InputStream inputStream, OutputStream outputStream, String outputCharset, String inputCharset, long limit) {
         try {
-            return IOUtils.copyLarge(inputStream, outputStream, 0, limit);
+            // Copy the buffer one-to-one if no charset conversion is required
+            if (outputCharset==null && inputCharset==null) {
+                return IOUtils.copyLarge(inputStream, outputStream, 0, limit);
+            }
+            else {
+                // Create readers to convert between character sets
+                InputStreamReader inputStreamReader = inputCharset==null ?
+                        new InputStreamReader(inputStream) : new InputStreamReader(inputStream, inputCharset);
+                OutputStreamWriter outputStreamWriter = outputCharset==null ?
+                        new OutputStreamWriter(outputStream) : new OutputStreamWriter(outputStream, outputCharset);
+                return IOUtils.copyLarge(inputStreamReader, outputStreamWriter, 0, limit);
+            }
         } catch (IOException e) {
             throw new OperationFailedException("write to stream", e.getMessage(), e);
         }
